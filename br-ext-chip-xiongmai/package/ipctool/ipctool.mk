523c5a9eafdbbdb801dd08b1df1abb889b63cd32
Igor Zalatov <flyrouter@gmail.com>
2021-04-17 20:56:11 +0300

    Add ipctool package

---
+++ br-ext-chip-xiongmai/package/ipctool/ipctool.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# ipctool
+#
+################################################################################
+
+IPCTOOL_VERSION = 04b2eec1f6d2cbb053548257836e6a7bbebf9d93
+IPCTOOL_SITE = $(call github,openipc,ipctool,$(IPCTOOL_VERSION))
+IPCTOOL_LICENSE = MIT
+IPCTOOL_LICENSE_FILES = LICENSE
+
+IPCTOOL_CONF_OPTS += -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DSKIP_VERSION=ON
+
+IPCTOOL_MAKE_OPTS += VERBOSE=1
+
+$(eval $(cmake-package))
