7d902be7ad16207f1cd26987bd7a7bb909a2eb32
Igor Zalatov <flyrouter@gmail.com>
2021-04-18 16:43:30 +0300

    Update xm530 files

--- br-ext-chip-xiongmai/external.mk
+++ br-ext-chip-xiongmai/external.mk
@@ -1,3 +1,8 @@
-include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai_patcher/xiongmai_patcher.mk
-include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv2/xiongmai-osdrv2.mk
 include $(BR2_EXTERNAL_XIONGMAI_PATH)/linux/linux-ext-xiongmai_patcher.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai_patcher/xiongmai_patcher.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/ipctool/ipctool.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/libevent-mini/libevent-mini.mk
+include $(BR2_EXTERNAL_XIONGMAI_PATH)/package/majestic/majestic.mk
+
