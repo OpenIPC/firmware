f384246d6a1b5350e89731dba2ea53b4d7b45f76
Igor Zalatov <flyrouter@gmail.com>
2021-04-26 22:17:49 +0300

    Up

--- br-ext-chip-sigmastar/package/sigmastar-osdrv-ssc335/files/script/load_modules.sh
+++ br-ext-chip-sigmastar/package/sigmastar-osdrv-ssc335/files/script/load_modules.sh
@@ -90,7 +90,7 @@ insert_ko()
 
     echo hvsp2 down /config/iqfile/filter.txt /config/iqfile/filter.txt > /sys/class/mstar/mscl/hvsp
 
-    insmod ${PATH_MODULE}/imx307_MIPI.ko chmap=1 lane_num=2 hdr_lane_num=2
+    #insmod ${PATH_MODULE}/imx307_MIPI.ko chmap=1 lane_num=2 hdr_lane_num=2
     mdev -s
 }
 
@@ -132,28 +132,3 @@ fi
 
 
 exit 0
-
-
-
-
-) 1280x 720,  30 fps max --> 192function:parese_Cmdline,pCmd_Section:0x3fe0000
-0x1080 [m--] WDR sn lx len:mmap->u32Size:0x7fe0000,but kernel len:0x3fe0000,fail!!!!
-
-нет ipu
-
- mi_ai 187375 0 - Live 0xbfb2f000 (PO)
- mi_divp 48340 0 - Live 0xbfb1e000 (PO)
-mi_disp 93595 0 - Live 0xbfaff000 (PO)
-mi_panel 24836 0 - Live 0xbfaf4000 (O)
- mi_venc 155868 0 - Live 0xbfac3000 (PO)
- mi_vif 39235 0 - Live 0xbfab4000 (PO)
-mi_gyro 11246 0 - Live 0xbfaad000 (PO)
- mi_shadow 34375 0 - Live 0xbfa9f000 (PO)
- mi_vpe 131267 0 - Live 0xbfa76000 (PO)
- mi_rgn 74250 2 mi_divp,mi_vpe, Live 0xbfa5d000 (PO)
- mi_ao 72914 0 - Live 0xbfa45000 (PO)
- mi_sensor 21952 0 - Live 0xbfa3b000 (PO)
- mi_sys 411756 12 mi_ai,mi_divp,mi_disp,mi_panel,mi_venc,mi_vif,mi_gyro,mi_shadow,mi_vpe,mi_rgn,mi_ao,mi_sensor, Live 0xbf9c2000 (PO)
- mi_common 5599 13 mi_ai,mi_divp,mi_disp,mi_panel,mi_venc,mi_vif,mi_gyro,mi_shadow,mi_vpe,mi_rgn,mi_ao,mi_sensor,mi_sys, Live 0xbf9bd000 (PO)
- mhal 1471673 12 mi_ai,mi_divp,mi_disp,mi_panel,mi_venc,mi_vif,mi_gyro,mi_vpe,mi_rgn,mi_ao,mi_sensor,mi_sys, Live 0xbf804000 (PO)
-ms_notify 1281 0 - Live 0xbf800000
