7a0030f9bc8650cab7b66593f24e6066de17b247
Igor Zalatov <flyrouter@gmail.com>
2021-05-03 17:47:33 +0300

    Update work

---
+++ general/package/node-exporter/node-exporter.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# node-exporter
+#
+################################################################################
+
+NODE_EXPORTER_VERSION =
+NODE_EXPORTER_SITE =
+NODE_EXPORTER_LICENSE = MIT
+NODE_EXPORTER_LICENSE_FILES = LICENSE
+NODE_EXPORTER_INSTALL_STAGING = YES
+
+
+define NODE_EXPORTER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(TOPDIR)/../general/package/node-exporter/files/script/S95prometheus
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(TOPDIR)/../general/package/node-exporter/files/script/prometheus-node-exporter-lua
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/lua/5.1/prometheus-collectors
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/lua/5.1/prometheus-collectors $(TOPDIR)/../general/package/node-exporter/files/prometheus-collectors/*.lua
+endef
+
+$(eval $(generic-package))
+
+
+
+
+
+
