648fa3d038473eb28e0dc19bb42d0299f45765e7
Igor Zalatov <flyrouter@gmail.com>
2021-05-04 23:07:49 +0300

    Fix * => 0.0.0.0

--- general/package/node-exporter/node-exporter.mk
+++ general/package/node-exporter/node-exporter.mk
@@ -13,10 +13,10 @@ NODE_EXPORTER_INSTALL_STAGING = YES
 
 define NODE_EXPORTER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(TOPDIR)/../general/package/node-exporter/files/script/S95prometheus
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(TOPDIR)/../general/package/node-exporter/files/script/S99node-exporter
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(TOPDIR)/../general/package/node-exporter/files/script/prometheus-node-exporter-lua
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(TOPDIR)/../general/package/node-exporter/files/script/node-exporter
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/lua/5.1/prometheus-collectors
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/lua/5.1/prometheus-collectors $(TOPDIR)/../general/package/node-exporter/files/prometheus-collectors/*.lua
