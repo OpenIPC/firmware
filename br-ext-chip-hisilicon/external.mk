519e19f134440651e8ab4742fe58c310fdf3a5f1
Igor Zalatov <flyrouter@gmail.com>
2021-05-12 15:16:55 +0300

    Add gdbserver-lite and enable CXX for hi3516ev300

--- br-ext-chip-hisilicon/external.mk
+++ br-ext-chip-hisilicon/external.mk
@@ -1,5 +1,6 @@
 include $(BR2_EXTERNAL_HISILICON_PATH)/linux/linux-ext-hisi_patcher.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_patcher/hisi_patcher.mk
+include $(BR2_EXTERNAL_HISILICON_PATH)/package/gdbserver-lite/gdbserver-lite.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_gpio_scanner/hisi_gpio_scanner.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv4/hisilicon-osdrv4.mk
