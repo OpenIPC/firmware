a5a5ba122eb426858db0bc45ff94f8cf5e01ba4f
Igor Zalatov <flyrouter@gmail.com>
2021-05-12 15:16:55 +0300

    Add gdbserver-lite and enable CXX for hi3516ev300

---
+++ general/package/gdbserver-lite/gdbserver-lite.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# gdbserver-lite | updated 2021.05.10
+#
+################################################################################
+
+GDBSERVER_LITE_VERSION = 5d973130b7e193d0c34e799e1272bec7d0e9917e
+GDBSERVER_LITE_SITE = $(call github,bet4it,gdbserver,$(GDBSERVER_LITE_VERSION))
+GDBSERVER_LITE_LICENSE = GNU General Public License v3.0
+GDBSERVER_LITE_LICENSE_FILES = LICENSE
+
+GDBSERVER_LITE_MAKE_OPTS = CROSS_COMPILE="$(TARGET_CROSS)" \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)" \
+	HOSTCFLAGS="$(HOST_CFLAGS)" \
+	STRIP=$(TARGET_STRIP) \
+	OBJS=gdbserver.o utils.o packets.o signals.o
+
+define GDBSERVER_LITE_BUILD_CMDS
+	(cd $(@D); $(TARGET_CC) -Os -std=gnu99 -s gdbserver.c utils.c packets.c signals.c -o gdbserver)
+endef
+
+define GDBSERVER_LITE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/gdbserver $(TARGET_DIR)/usr/sbin/gdbserver
+endef
+
+$(eval $(generic-package))
