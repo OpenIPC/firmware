64a73486bcd31da01d53d376ec9966b1e73aa765
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-06-19 19:53:15 +0300

    fix yaml-cli.mk and up to v0.0.3

--- general/package/yaml-cli/yaml-cli.mk
+++ general/package/yaml-cli/yaml-cli.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-YAML_CLI_VERSION = 0.0.2
+YAML_CLI_VERSION = 0.0.3
 YAML_CLI_SITE = $(call github,openipc,yaml-cli,v$(YAML_CLI_VERSION))
 YAML_CLI_LICENSE = MIT
 YAML_CLI_LICENSE_FILES = LICENSE
 
 YAML_CLI_DEPENDENCIES = libyaml
-YAML_CLI_CONF_OPTS += -DCMAKE_BUILD_TYPE=MinSizeRel
+YAML_CLI_CONF_OPTS += -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_BINDIR="/usr/bin"
 
 YAML_CLI_MAKE_OPTS += VERBOSE=1
 
