a69c0f961c36fd2e9da4ea2f8f19e20cfb36d222
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-06-20 23:20:10 +0300

    Fullhan init

---
+++ br-ext-chip-fullhan/external.mk
@@ -0,0 +1,10 @@
+include $(BR2_EXTERNAL_FULLHAN_PATH)/linux/linux-ext-fullhan_patcher.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/fullhan_patcher/fullhan_patcher.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/aura-httpd/aura-httpd.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/ipctool/ipctool.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/libevent-mini/libevent-mini.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/majestic/majestic.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/node-exporter/node-exporter.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/vtund-lite/vtund-lite.mk
+include $(BR2_EXTERNAL_FULLHAN_PATH)/package/yaml-cli/yaml-cli.mk
