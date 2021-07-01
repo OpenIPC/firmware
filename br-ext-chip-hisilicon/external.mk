85835a14e365f47e5fcc9b16c522fe25881e83f3
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-07-01 20:02:33 +0300

    Fix hisi_gpio

--- br-ext-chip-hisilicon/external.mk
+++ br-ext-chip-hisilicon/external.mk
@@ -3,7 +3,7 @@ include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_patcher/hisi_patcher.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/aura-httpd/aura-httpd.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/gdbserver-lite/gdbserver-lite.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
-include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_gpio/hisi_gpio_scanner.mk
+include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_gpio/hisi_gpio.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv4/hisilicon-osdrv4.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/ipctool/ipctool.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/libevent-mini/libevent-mini.mk
