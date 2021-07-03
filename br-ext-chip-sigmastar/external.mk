bc5624828689bbe96d2382855cf2f15613039655
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-07-03 22:53:35 +0300

    Add libsrt to project

--- br-ext-chip-sigmastar/external.mk
+++ br-ext-chip-sigmastar/external.mk
@@ -5,6 +5,7 @@ include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/gdbserver-lite/gdbserver-lite.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/ipctool/ipctool.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/libevent-mini/libevent-mini.mk
+include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/libsrt/libsrt.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/node-exporter/node-exporter.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/sigmastar-osdrv-ssc335/sigmastar-osdrv-ssc335.mk
 include $(BR2_EXTERNAL_SIGMASTAR_PATH)/package/vtund-lite/vtund-lite.mk
