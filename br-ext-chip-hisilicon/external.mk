80466e0915813a9b6b92b69f9c5978da22fdfdb3
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-07-04 17:58:10 +0300

    Fix Hisilicon repo building

--- br-ext-chip-hisilicon/external.mk
+++ br-ext-chip-hisilicon/external.mk
@@ -7,7 +7,7 @@ include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisi_gpio/hisi_gpio.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv4/hisilicon-osdrv4.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/ipctool/ipctool.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/libevent-mini/libevent-mini.mk
-include $(BR2_EXTERNAL_HISILICON_PATH)/package/libhvt/libhv.mk
+include $(BR2_EXTERNAL_HISILICON_PATH)/package/libhv/libhv.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/libsrt/libsrt.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/majestic/majestic.mk
 include $(BR2_EXTERNAL_HISILICON_PATH)/package/node-exporter/node-exporter.mk
