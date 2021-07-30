a69109b1c3dc44458eed287dab72224ddf2fb972
Igor Zalatov (via Araneus server) <flyrouter@gmail.com>
2021-07-30 17:53:29 +0300

    Support for Goke processors has started

---
+++ br-ext-chip-goke/external.mk
@@ -0,0 +1,12 @@
+include $(BR2_EXTERNAL_GOKE_PATH)/linux/linux-ext-goke_patcher.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/goke_patcher/goke_patcher.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/aura-httpd/aura-httpd.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/fwprintenv-ng/fwprintenv-ng.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/ipctool/ipctool.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/libevent-mini/libevent-mini.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/libhv/libhv.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/libsrt/libsrt.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/majestic/majestic.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/node-exporter/node-exporter.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/vtund-lite/vtund-lite.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/yaml-cli/yaml-cli.mk
