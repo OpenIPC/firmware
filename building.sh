6305cc8f26296611f614b2f3f818e22b145e1020
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-08-08 13:54:24 +0300

    Cleanup and sync configs

--- building.sh
+++ building.sh
@@ -72,19 +72,9 @@ hi3516ev300() {
   fresh && make PLATFORM=hisilicon BOARD=unknown_unknown_hi3516ev300_openipc all && rename
 }
 
-hi3516ev300_dev() {
-  soc="hi3516ev300"
-  fresh && make PLATFORM=hisilicon BOARD=unknown_unknown_hi3516ev300_dev all && rename
-}
-
 hi3516ev300_glibc() {
   soc="hi3516ev300"
   fresh && make PLATFORM=hisilicon BOARD=unknown_unknown_hi3516ev300_glibc all && rename
-}
-
-hi3516ev300_opus() {
-  soc="hi3516ev300"
-  fresh && make PLATFORM=hisilicon BOARD=unknown_unknown_hi3516ev300_opus all && rename
 }
 
 hi3516ev300_tehshield() {
@@ -143,11 +133,10 @@ xm530() {
 # hi3516ev300                   # OK
 # hi3516ev300_dev               # testing..
 # hi3516ev300_glibc             # testing..
-hi3516ev300_opus              # OPUS testing
 # hi3516ev300_tehshield         # Tehshield
 #
 # ssc335                        # OpenIPC
-# ssc335_blackbird              # BlackBird/Sputnik
+ssc335_blackbird              # BlackBird/Sputnik
 # ssc335_goodcam                # GoodCam
 # ssc335_musl                   # Musl
 # ssc335_rotek                  # Rotek
