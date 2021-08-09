cedae711408dba6f3bab65fc5cb1830bcfe1d46c
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-08-10 00:28:14 +0300

    Add fdk-aac-openipc package

--- br-ext-chip-goke/external.mk
+++ br-ext-chip-goke/external.mk
@@ -1,6 +1,7 @@
 include $(BR2_EXTERNAL_GOKE_PATH)/linux/linux-ext-goke_patcher.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/goke_patcher/goke_patcher.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/aura-httpd/aura-httpd.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/fdk-aac-openipc/fdk-aac-openipc.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/fwprintenv-openipc/fwprintenv-openipc.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/ipctool/ipctool.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/lame-openipc/lame-openipc.mk
---
+++ general/package/fdk-aac-openipc/fdk-aac-openipc.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# fdk-aac-openipc
+#
+################################################################################
+
+FDK_AAC_OPENIPC_VERSION = 2.0.2
+FDK_AAC_OPENIPC_SITE = $(call github,mstorsjo,fdk-aac,v$(FDK_AAC_OPENIPC_VERSION))
+FDK_AAC_OPENIPC_LICENSE = fdk-aac license
+FDK_AAC_OPENIPC_LICENSE_FILES = NOTICE
+FDK_AAC_OPENIPC_INSTALL_STAGING = YES
+
+# This package uses autoconf, but not automake, so we need to call
+# their special autogen.sh script, and have custom target and staging
+# installation commands.
+
+define FDK_AAC_OPENIPC_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+FDK_AAC_OPENIPC_PRE_CONFIGURE_HOOKS += FDK_AAC_OPENIPC_RUN_AUTOGEN
+
+
+$(eval $(autotools-package))
