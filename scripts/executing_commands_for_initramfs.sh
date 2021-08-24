a91e9dc66933bd9765438ed433fcef0d04a38fc4
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-08-24 14:18:19 +0300

    Fix Hi3516Av300 building and some initramfs changes

--- scripts/executing_commands_for_initramfs.sh
+++ scripts/executing_commands_for_initramfs.sh
@@ -7,6 +7,7 @@ echo 'Note: Deleting unused files from the image..'
 #
 rm -f ${TARGET_DIR}/etc/init.d/S01syslogd
 rm -f ${TARGET_DIR}/etc/init.d/S10modules
+rm -f ${TARGET_DIR}/etc/init.d/S20urandom
 rm -f ${TARGET_DIR}/etc/init.d/S21rngd
 rm -f ${TARGET_DIR}/etc/init.d/S40mdev
 rm -f ${TARGET_DIR}/etc/init.d/S49ntpd
@@ -14,4 +15,14 @@ rm -f ${TARGET_DIR}/etc/init.d/S50dropbear
 rm -f ${TARGET_DIR}/etc/init.d/S50snmpd
 #
 rm -rf ${TARGET_DIR}/etc/ssl
+#
+rm -f ${TARGET_DIR}/lib/libatomic*
+rm -f ${TARGET_DIR}/lib/libgcc*
+#
+#rm -rf ${TARGET_DIR}/lib/modules
+#
+rm -rf ${TARGET_DIR}/usr/sbin/firstboot
+rm -rf ${TARGET_DIR}/usr/sbin/sysupgrade
+#
+# mkdir -p ${TARGET_DIR}/dev ; mknod ${TARGET_DIR}/dev/null c 3 2; chmod 666 ${TARGET_DIR}/dev/null
 #
