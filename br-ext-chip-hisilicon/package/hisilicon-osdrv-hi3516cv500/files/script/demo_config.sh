43b929fce57f18af97c618ae89176a514aeead69
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-08-27 18:35:15 +0300

    Update hi3516cv500 project

--- br-ext-chip-hisilicon/package/hisilicon-osdrv-hi3516cv500/files/script/demo_config.sh
+++ br-ext-chip-hisilicon/package/hisilicon-osdrv-hi3516cv500/files/script/demo_config.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+# Desciption:ir cut enable or disable
+# Copyright (C) Hisilicon Technologies Co., Ltd. 2012-2019. All rights reserved.
+
+set -e
 
 #GPIO3_3 -> GPIO27 (3*8+3 = 27)
 #GPIO3_4 -> GPIO28 (3*8+4 = 28)
@@ -60,12 +64,12 @@ ir_cut_disable()
 if [ $# -eq 0 ]; then
     echo "ir mode : ./demo_config.sh 1";
 else
-    if [ $1 -eq 0 ]; then
+    if [ "$1" -eq "0" ]; then
         echo "normal mode, ir_cut on"
         ir_cut_enable > /dev/null;
     fi
 
-    if [ $1 -eq 1 ]; then
+    if [ "$1" -eq "1" ]; then
         echo "ir mode, ir_cut off"
         ir_cut_disable > /dev/null;
     fi
