6be04af447c1ad5af81f0c52947e56e8d8b906c0
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-08-30 23:51:19 +0300

    Update ipctool, profile and rtc patches

--- general/package/ipctool/ipctool.mk
+++ general/package/ipctool/ipctool.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# ipctool | updated 2021.08.29
+# ipctool | updated 2021.08.30
 #
 ################################################################################
 
-IPCTOOL_VERSION = 0fceea1e8975c4fb45f6cd1788cf41aabea29dc8
+IPCTOOL_VERSION = 7f97012a950aea758d2df827a0772dacc4c8bc99
 IPCTOOL_SITE = $(call github,openipc,ipctool,$(IPCTOOL_VERSION))
 IPCTOOL_LICENSE = MIT
 IPCTOOL_LICENSE_FILES = LICENSE
