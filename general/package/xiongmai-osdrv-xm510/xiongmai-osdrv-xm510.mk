fe769643af29268c23642ce665be6d6779cedd23
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-09-23 08:43:50 +0300

    Update XM510/XM530 profiles

--- general/package/xiongmai-osdrv-xm510/xiongmai-osdrv-xm510.mk
+++ general/package/xiongmai-osdrv-xm510/xiongmai-osdrv-xm510.mk
@@ -27,18 +27,17 @@ endef
 
 
 define XIONGMAI_OSDRV_XM510_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
+	# $(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
 	# $(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/sensor/config/*.ini
 
-	# $(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.0.101/xiongmai
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.101/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/kmod/*.ko
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.101/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/kmod/usb/*.ko
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.0.101/xiongmai
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.101/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/kmod/*.ko
 
-	# $(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	# $(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/script/*
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/script/*
 
-	# $(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/fonts
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/fonts $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/fonts/*.bin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/fonts
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/fonts $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/fonts/*.bin
 
 	# $(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
 	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/sensor/*.so
