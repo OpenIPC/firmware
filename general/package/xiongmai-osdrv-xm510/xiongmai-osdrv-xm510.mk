eef77653d5b13c72475588fa1227f42416a6810b
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-09-23 09:12:21 +0300

    Fix path for XM510/XM530

--- general/package/xiongmai-osdrv-xm510/xiongmai-osdrv-xm510.mk
+++ general/package/xiongmai-osdrv-xm510/xiongmai-osdrv-xm510.mk
@@ -34,7 +34,7 @@ define XIONGMAI_OSDRV_XM510_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.101/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/kmod/*.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/script/*
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/script/load*
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/fonts
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/fonts $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm510/files/fonts/*.bin
--- general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
+++ general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
@@ -41,7 +41,7 @@ define XIONGMAI_OSDRV_XM530_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/kmod/usb/*.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/script/*
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/script/load*
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/fonts
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/fonts $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/fonts/*.bin
