9321fd8a3350e2d7ba7fdce078193b881d5bba20
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-10-02 20:49:43 +0300

    Fix xmdp

--- general/package/xmdp/xmdp.mk
+++ general/package/xmdp/xmdp.mk
@@ -12,8 +12,8 @@ endef
 
 define XMDP_BUILD_CMDS
 	(cd $(@D); $(TARGET_CC) -Wall -Os -s mcookie.c -o mcookie)
-<------>$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -O -g -D LINUX -std=c99 -Wall -Wpedantic -lm -o $(PKG_BUILD_DIR)/$(PKG_NAME) \
-<------>$(PKG_BUILD_DIR)/xmdp.c $(PKG_BUILD_DIR)/netip.c $(PKG_BUILD_DIR)/utils.c $(PKG_BUILD_DIR)/cjson/cJSON.c
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -O -g -D LINUX -std=c99 -Wall -Wpedantic -lm -o $(PKG_BUILD_DIR)/$(PKG_NAME) \
+	$(PKG_BUILD_DIR)/xmdp.c $(PKG_BUILD_DIR)/netip.c $(PKG_BUILD_DIR)/utils.c $(PKG_BUILD_DIR)/cjson/cJSON.c
 endef
 
 define XMDP_INSTALL_TARGET_CMDS
