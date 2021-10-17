7269bdf9b052574aa3efbf93a8547a8a0d9bd5b6
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-10-17 18:15:38 +0300

    Update Hisilicon scripts

--- general/package/hisilicon-osdrv-hi3516ev300/hisilicon-osdrv-hi3516ev300.mk
+++ general/package/hisilicon-osdrv-hi3516ev300/hisilicon-osdrv-hi3516ev300.mk
@@ -69,7 +69,7 @@ define HISILICON_OSDRV_HI3516EV300_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/kmod/sys_config.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/script/load*
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/script/load_hisilicon
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/script/ircut_demo
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/script/set_allocator
 
