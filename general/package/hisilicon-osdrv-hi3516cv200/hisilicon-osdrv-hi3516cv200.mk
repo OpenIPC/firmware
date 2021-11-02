f176dd2d642abd9e780ebf4a7cc827d3765e9b8c
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-11-02 22:33:23 +0300

    Fix Hisilicom compiling trouble

---
+++ general/package/hisilicon-osdrv-hi3516cv200/hisilicon-osdrv-hi3516cv200.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# hisilicon-osdrv-hi3516cv200
+#
+################################################################################
+
+HISILICON_OSDRV_HI3516CV200_VERSION =
+HISILICON_OSDRV_HI3516CV200_SITE =
+HISILICON_OSDRV_HI3516CV200_LICENSE = MIT
+HISILICON_OSDRV_HI3516CV200_LICENSE_FILES = LICENSE
+
+
+define HISILICON_OSDRV_HI3516CV200_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/script/S99hisilicon
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/sensor/config/*.ini
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.37/hisilicon
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/kmod/*.ko
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/kmod/xxx.ko
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/script/load_hisilicon
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/script/ircut_demo
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/fonts
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/fonts $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/fonts/*.bin
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/sensor/*.so
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/lib/*.so
+	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516cv200/files/lib/xxx.so
+endef
+
+$(eval $(generic-package))
