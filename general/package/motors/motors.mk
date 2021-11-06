f416acdff7673989b17b49df06c65c03696f7b19
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-11-06 21:55:26 +0300

    Add CamHi P/T motor sample

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -1,20 +1,22 @@
 ################################################################################
 #
-# motors | updated 2021.10.02
+# motors | updated 2021.11.06
 #
 ################################################################################
 
-MOTORS_VERSION = 686c50a31370dcbb1a71fc8b499f247868a52436
+MOTORS_VERSION = 32ba1309e739c068a89132aaebcde69b71fdaf4a
 MOTORS_SITE = $(call github,openipc,motors,$(MOTORS_VERSION))
 MOTORS_LICENSE = MIT
 MOTORS_LICENSE_FILES = LICENSE
 
 define MOTORS_BUILD_CMDS
+	(cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor)
 	(cd $(@D)/xm-kmotor; $(TARGET_CC) -Os -s main.c -o xm-kmotor)
 	(cd $(@D)/xm-uart; $(TARGET_CC) -Os -s main.c -o xm-uart)
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor
 	$(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor
 	$(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart
 endef
