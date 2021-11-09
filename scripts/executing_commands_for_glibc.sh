40765fcc26ac3a39f559c5066964ba10788efac8
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-11-10 00:18:58 +0300

    git status

--- scripts/executing_commands_for_glibc.sh
+++ scripts/executing_commands_for_glibc.sh
@@ -3,7 +3,7 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
-echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="glibc"'
-#
 date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+#
+echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="glibc"'
 #
--- scripts/executing_commands_for_musl.sh
+++ scripts/executing_commands_for_musl.sh
@@ -3,10 +3,13 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
+date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+#
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="musl"'
 #
 ln -sfv /lib/libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
 ln -sfv ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
 #
-date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+# Comment out this line if you want to save the libraries
+rm -f ${TARGET_DIR}/usr/lib/libstdc++*
 #
