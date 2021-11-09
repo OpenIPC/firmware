3a41337bcb66ea6f80f23989ac19f34d592a32ca
p0isk <radio.86rk@gmail.com>
2021-11-09 04:02:58 +0300

    Update microbe-web.mk

--- general/package/microbe-web/microbe-web.mk
+++ general/package/microbe-web/microbe-web.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# microbe-web | updated 2021.10.17
+# microbe-web | updated 2021.11.09
 #
 ################################################################################
 
-MICROBE_WEB_VERSION = 7203cf1eaeddbab7453e9f02da0a539428cc0f73
+MICROBE_WEB_VERSION = 09c6b2b9702b909daa6a27d2514dddb22383acbf
 MICROBE_WEB_SITE = $(call github,openipc,microbe-web,$(MICROBE_WEB_VERSION))
 MICROBE_WEB_LICENSE = MIT
 MICROBE_WEB_LICENSE_FILES = LICENSE
