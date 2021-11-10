af86632f04eb2da9d16db691a79ad56b5fc2a98e
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-11-10 22:18:31 +0300

    Dirty hack for XM530+XM711

--- general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
+++ general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
@@ -25,6 +25,9 @@ define XIONGMAI_OSDRV_XM530_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/kmod/usb/*.ko
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/kmod/xm711/*.ko
 
+# Dirty hack
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/kernel/net/wireless $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/kmod/rewrite/cfg80211.ko
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(BR2_EXTERNAL_XIONGMAI_PATH)/package/xiongmai-osdrv-xm530/files/script/load*
 
