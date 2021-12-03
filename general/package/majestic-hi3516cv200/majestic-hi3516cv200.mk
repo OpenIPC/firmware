d509abc9870d262268f9925eb9939dfe5027aea7
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-12-03 23:38:38 +0300

    Update actions

--- general/package/majestic-hi3516cv200/majestic-hi3516cv200.mk
+++ general/package/majestic-hi3516cv200/majestic-hi3516cv200.mk
@@ -12,7 +12,7 @@ MAJESTIC_HI3516CV200_LICENSE_FILES = LICENSE
 
 define MAJESTIC_HI3516CV200_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/majestic.yaml
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/majestic.yaml $(@D)/majestic-mini.yaml
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/majestic
