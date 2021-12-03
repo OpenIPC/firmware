4750fdc6b02422bbf99e1f83dae31ff15c669ac2
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-12-04 00:44:59 +0300

    Go to simple Majestic config

--- general/package/majestic-nt9856x/majestic-nt9856x.mk
+++ general/package/majestic-nt9856x/majestic-nt9856x.mk
@@ -12,7 +12,7 @@ MAJESTIC_NT9856X_LICENSE_FILES = LICENSE
 
 define MAJESTIC_NT9856X_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/majestic.yaml
+	$(INSTALL) -m 644 $(@D)/majestic-mini.yaml $(TARGET_DIR)/etc/majestic.yaml
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/majestic
