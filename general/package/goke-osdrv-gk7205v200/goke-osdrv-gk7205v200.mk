70f516997e6e498054af68356c22de15ac3d7e77
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-12-03 13:13:02 +0300

    Enable IMX335 support

--- general/package/goke-osdrv-gk7205v200/goke-osdrv-gk7205v200.mk
+++ general/package/goke-osdrv-gk7205v200/goke-osdrv-gk7205v200.mk
@@ -34,7 +34,7 @@ define GOKE_OSDRV_GK7205V200_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/config/*.ini
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/WDR
-	#$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/WDR $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/config/WDR/*.ini
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/WDR $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/config/WDR/*.ini
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.37/goke
 	## $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/goke $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/kmod/cipher_drv.ko
