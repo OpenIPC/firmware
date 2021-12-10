57502034c9cd67a8c182e8c56cc69c67e3d49d29
p0isk <radio.86rk@gmail.com>
2021-12-10 15:59:56 +0300

    Add OPENIPC_VERSION

--- scripts/executing_commands_for_musl.sh
+++ scripts/executing_commands_for_musl.sh
@@ -3,7 +3,9 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
-date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+GITHUB_VERSION="${BRANCH_NAME}+${GIT_HASH}" 
+RELEASE_DATE=$(date "+%y.%m.%d") 
+echo "OPENIPC_VERSION=${GITHUB_VERSION}, 2.${RELEASE_DATE:1}" >>${TARGET_DIR}/usr/lib/os-release
 #
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="musl"'
 #
