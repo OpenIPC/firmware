56d9b1842383dc0b80f3547f16f6ec64fdb65a11
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2021-12-15 19:35:09 +0300

    Add package json-c-openipc

---
+++ general/package/json-c-openipc/json-c-openipc.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# json-c-openipc
+#
+################################################################################
+
+JSON_C_OPENIPC_VERSION = json-c-0.15-20200726
+JSON_C_OPENIPC_SITE = $(call github,json-c,json-c,v$(JSON_C_OPENIPC_VERSION))
+JSON_C_OPENIPC_INSTALL_STAGING = YES
+JSON_C_OPENIPC_LICENSE = MIT
+JSON_C_OPENIPC_LICENSE_FILES = COPYING
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
