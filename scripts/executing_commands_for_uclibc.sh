779d7658ea0918c798f47b0c918fa349c166a291
Dmitry Ilyin <dima@doty.ru>
2021-12-31 12:46:11 +0300

    Bump OpenIPC version to 2.2 (#133)

--- scripts/executing_commands_for_uclibc.sh
+++ scripts/executing_commands_for_uclibc.sh
@@ -3,7 +3,7 @@
 # Please check BR2_ROOTFS_POST_BUILD_SCRIPT option in config
 #
 
-date "+OPENIPC_VERSION=2.1.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
+date "+OPENIPC_VERSION=2.2.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
 date "+GITHUB_VERSION=\"${BRANCH_NAME}+${GIT_HASH}, %Y-%m-%d\"" >>${TARGET_DIR}/usr/lib/os-release
 #
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="uclibc"'
