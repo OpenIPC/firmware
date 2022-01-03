bfd2ab2661de553220032ac1153fc626461b2c57
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-01-03 18:58:38 +0300

    Build option examples

--- scripts/executing_commands_for_musl.sh
+++ scripts/executing_commands_for_musl.sh
@@ -5,6 +5,7 @@
 
 date "+OPENIPC_VERSION=2.2.%m.%d" >>${TARGET_DIR}/usr/lib/os-release
 date "+GITHUB_VERSION=\"${BRANCH_NAME}+${GIT_HASH}, %Y-%m-%d\"" >>${TARGET_DIR}/usr/lib/os-release
+echo "BUILD_OPTION=lite" >>${TARGET_DIR}/usr/lib/os-release
 #
 echo 'Note: BR2_TOOLCHAIN_BUILDROOT_LIBC="musl"'
 #
