0e3419d1c9fe6a14c44aefee8aa2b047822cb403
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-01-03 19:21:47 +0300

    Remove ipctool from firmware

--- general/package/ipctool/ipctool.mk
+++ general/package/ipctool/ipctool.mk
@@ -13,4 +13,10 @@ IPCTOOL_CONF_OPTS += -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DSKIP_VE
 
 IPCTOOL_MAKE_OPTS += VERBOSE=1
 
+define IPCTOOL_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/ipcinfo
+endef
+
+
 $(eval $(cmake-package))
