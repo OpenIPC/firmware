655e88e336da9de99c9cba4ccfc4405ee29c08a5
Igor Zalatov <zig@dolphin.flymon.net>
2022-01-04 20:08:52 +0300

    Update openhisilicon package

--- general/package/openhisilicon/openhisilicon.mk
+++ general/package/openhisilicon/openhisilicon.mk
@@ -4,19 +4,19 @@
 #
 ################################################################################
 
-#OPENHISILICON_VERSION = HEAD
-#OPENHISILICON_SITE = $(call github,openipc,openhisilicon,$(OPENHISILICON_VERSION))
-OPENHISILICON_LICENSE = GPL-2.0
-OPENHISILICON_LICENSE_FILES = COPYING
+OPENHISILICON_VERSION = HEAD
+OPENHISILICON_SITE = $(call github,openipc,openhisilicon,$(OPENHISILICON_VERSION))
+OPENHISILICON_LICENSE = GPL-3.0
+OPENHISILICON_LICENSE_FILES = LICENSE
 
 OPENHISILICON_MODULE_MAKE_OPTS = \
 	CONFIG_HWRNG=m \
 	KVER=$(LINUX_VERSION_PROBED) \
 	KSRC=$(LINUX_DIR)
 
-define OPENHISILICON_EXTRACT_CMDS
-	cp -v $(BR2_EXTERNAL_HISILICON_PATH)/package/openhisilicon/src/* $(@D)/
-endef
+#define OPENHISILICON_EXTRACT_CMDS
+#	cp -v $(BR2_EXTERNAL_HISILICON_PATH)/package/openhisilicon/src/* $(@D)/
+#endef
 
 
 
