79781c4564c74299409575facbcdee5d303257d2
Dmitry Ermakov <de@dimer.org.ua>
2022-01-08 22:17:02 +0300

    Add sensor profiles for NT9856x Add sc501ai built from source

--- general/package/novatek-osdrv-nt9856x/novatek-osdrv-nt9856x.mk
+++ general/package/novatek-osdrv-nt9856x/novatek-osdrv-nt9856x.mk
@@ -17,6 +17,9 @@ define NOVATEK_OSDRV_NT9856X_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/config/sen_sc*.cfg
 
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/profiles
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/profiles $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/profiles/isp*.cfg
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.19.91/novatek/hdal
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.19.91/novatek/hdal $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/kmod/hdal/kwrap.ko
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.19.91/novatek/hdal/comm/nvtmem
@@ -78,7 +81,7 @@ define NOVATEK_OSDRV_NT9856X_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.19.91/novatek $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/sen_sc401ai/*.ko
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.19.91/novatek $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/sen_sc500ai/*.ko
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.19.91/novatek $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/sen_sc501ai/*.ko
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.19.91/novatek $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/sensor/sen_sc501ai/*.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(BR2_EXTERNAL_NOVATEK_PATH)/package/novatek-osdrv-nt9856x/files/lib/libhdal.so
