3fa311f5090b467e9f4eb5ddad4991ae44c305e5
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-02-10 01:26:30 +0300

    Add UQMI package

---
+++ general/package/uqmi/uqmi.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# uqmi
+#
+################################################################################
+
+UQMI_VERSION = f254fc59c710d781eca3ec36e0bff2d8970370fa
+UQMI_SITE = git://git.openwrt.org/project/uqmi.git
+UQMI_DEPENDENCIES = json-c-openipc libubox
+UQMI_LICENSE = LGPL-2.0+
+UQMI_LICENSE_FILES = COPYING
+
+$(eval $(cmake-package))
