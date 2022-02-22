14c27dbf24fa0d7544efdc486891a320677b2569
cronyx <xcronyx@gmail.com>
2022-02-22 12:40:48 +0300

    update wfb readme

--- scripts/executing_commands_for_musl.sh
+++ scripts/executing_commands_for_musl.sh
@@ -13,5 +13,5 @@ ln -sfv /lib/libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
 ln -sfv ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
 #
 # Comment out this line if you want to save the libraries
-rm -f ${TARGET_DIR}/usr/lib/libstdc++*
+# rm -f ${TARGET_DIR}/usr/lib/libstdc++*
 #
