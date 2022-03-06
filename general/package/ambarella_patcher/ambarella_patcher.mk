cb40a2e47382711554e2558dea39472bb3c364ce
Dmitry Ermakov <de@dimer.org.ua>
2022-03-06 21:49:30 +0300

    Add Ambarella S3L

---
+++ general/package/ambarella_patcher/ambarella_patcher.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# ambarella_patcher
+#
+################################################################################
+
+AMBARELLA_PATCHER_VERSION = 0.1
+AMBARELLA_PATCHER_INSTALL_TARGET = NO
+AMBARELLA_PATCHER_INSTALL_STAGING = NO
+AMBARELLA_PATCHER_SITE =
+AMBARELLA_PATCHER_SOURCE =
+
+$(eval $(generic-package))
