7e70bd23fcb676b6cf07bedcfd2d5fa2f480c936
cronyx <cronyx@nodasoft.com>
2022-07-13 21:21:48 +0300

    add some comments

--- building.sh
+++ building.sh
@@ -649,7 +649,7 @@ xm550() {
 # gk7205v210                    # OpenIPC
 # gk7205v300                    # OpenIPC
 # gk7205v300_ultimate           # OpenIPC_ultimate version
-gk7205v300_fpv                # FPV (ultimate by default)
+# gk7205v300_fpv                # FPV (ultimate by default)
 # gk7605v100                    # testing..
 #
 #######
