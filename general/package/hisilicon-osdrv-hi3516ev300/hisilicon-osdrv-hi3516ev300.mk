e1a4dad9f020a635be4abd047de170fa658ad3fe
Dmitry Ermakov <de@dimer.org.ua>
2022-07-27 01:08:35 +0300

    IQ fixes

--- general/package/hisilicon-osdrv-hi3516ev300/hisilicon-osdrv-hi3516ev300.mk
+++ general/package/hisilicon-osdrv-hi3516ev300/hisilicon-osdrv-hi3516ev300.mk
@@ -22,6 +22,7 @@ define HISILICON_OSDRV_HI3516EV300_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/iq
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iq $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/sensor/iq/imx307.ini
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iq $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/sensor/iq/imx335.ini
+	ln -sf imx335.ini $(TARGET_DIR)/etc/sensors/iq/default.ini
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.37/hisilicon
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon $(BR2_EXTERNAL_HISILICON_PATH)/package/hisilicon-osdrv-hi3516ev300/files/kmod/camhi-motor.ko
