a247be9a612ae09dcf1b45fb6443f39a1c2f556f
cronyx <xcronyx@gmail.com>
2022-07-29 23:06:51 +0300

    rollback changes in local build script

--- building.sh
+++ building.sh
@@ -667,7 +667,7 @@ xm550() {
 #
 # hi3516cv200                   # testing..
 # hi3516cv200                   # testing..
-hi3518ev200                   # OpenIPC
+# hi3518ev200                   # OpenIPC
 # hi3518ev200_domsip            # DomSip
 # hi3518ev200_hs303             # OpenIPC
 # hi3518ev200_ultimate          # OpenIPC_ultimate version
