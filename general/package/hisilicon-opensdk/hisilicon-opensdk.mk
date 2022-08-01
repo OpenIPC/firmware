c53f80d6892d9a0d9d1b8f348d04be4be8b2f7b0
Dmitry Ilyin <dima@doty.ru>
2022-08-01 17:39:22 +0300

    Enabled OpenHiSilicon SDK for Goke new generation

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -4,17 +4,10 @@
 #
 ################################################################################
 
-HISILICON_OPENSDK_VERSION = 164afc7dc6b322b3d3175d9eb5cfe1c81de3c00b
-HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(MOTORS_VERSION))
-HISILICON_OPENSDK_LICENSE = GPL-3.0
+HISILICON_OPENSDK_VERSION = 7cc0bab2d40c0b35693f541cacd1bcb85d2c63b6
+HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
+HISILICON_OPENSDK_LICENSE = GPL-2.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
 
-define HISILICON_OPENSDK_BUILD_CMDS
-	# (cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor)
-endef
-
-define HISILICON_OPENSDK_INSTALL_TARGET_CMDS
-	# $(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
