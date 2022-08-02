4e0901a9f189373a35d2e93279ba2f8ed0ce0edb
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-08-02 14:37:42 +0300

    Symlink OpenSDK from Hisilicon to Goke

--- br-ext-chip-goke/external.mk
+++ br-ext-chip-goke/external.mk
@@ -5,6 +5,7 @@ include $(BR2_EXTERNAL_GOKE_PATH)/package/baresip-openipc/baresip-openipc.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/dropbear-openipc/dropbear-openipc.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/fdk-aac-openipc/fdk-aac-openipc.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/fwprintenv-openipc/fwprintenv-openipc.mk
+include $(BR2_EXTERNAL_GOKE_PATH)/package/goke-opensdk/hisilicon-opensdk.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/goke-osdrv-gk710x.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/goke-osdrv-gk7205v200.mk
 include $(BR2_EXTERNAL_GOKE_PATH)/package/ipctool/ipctool.mk
