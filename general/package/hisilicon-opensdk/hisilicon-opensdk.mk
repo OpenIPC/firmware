8b0374adc11ca024426ada848be36f37a05ed6fd
Dmitry Ilyin <dima@doty.ru>
2022-08-03 14:12:50 +0300

    Enabled OpenHiSilicon SDK for Goke new generation (#303)
    * Enabled OpenHiSilicon SDK for Goke new generation
    
    * Change package name
    
    * Resolve conflicts
    
    * Fix version
    
    * Fix version

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -1,17 +1,24 @@
 ################################################################################
 #
-# hisilicon-opensdk | updated 2022.08.02
+# openhisilicon
 #
 ################################################################################
 
-HISILICON_OPENSDK_VERSION = HEAD
+HISILICON_OPENSDK_VERSION = fc315a59157e20654f2a572d24b1f973edaa6588
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
 
+ifndef BOARD
+	@$$(call MESSAGE,"Variable BOARD must be defined")
+	@exit
+else
+	CHIP := $(shell echo $(BOARD) | cut -d "_" -f 3)
+endif
+
+HISILICON_OPENSDK_MODULE_SUBDIRS = kernel
 HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
-	KVER=$(LINUX_VERSION_PROBED) \
-	KSRC=$(LINUX_DIR)
+	CHIPSET=$(CHIP)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
