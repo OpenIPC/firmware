a9ecf12a7dc4bb54bd8fcb0d735c404a36c8b40a
Dmitry Ilyin <dima@doty.ru>
2022-08-03 11:31:11 +0300

    Fix version

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+HISILICON_OPENSDK_VERSION = HEAD
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
