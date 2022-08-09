6ba955dc9f6734e560a6309ee18b8243725b011c
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-08-09 18:05:23 +0300

    Add example daemon

---
+++ general/package/ax-agent/ax-agent.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# ax-agent | updated 2022.08.09
+#
+################################################################################
+
+AX_AGENT_LICENSE = MIT
+AX_AGENT_LICENSE_FILES = LICENSE
+
+define AX_AGENT_EXTRACT_CMDS
+	cp $(BR2_EXTERNAL_GOKE_PATH)/package/ax-agent/src/ax-agent.c $(@D)/
+endef
+
+define AX_AGENT_BUILD_CMDS
+	(cd $(@D); $(TARGET_CC) -s ax-agent.c -o ax-agent)
+endef
+
+define AX_AGENT_INSTALL_TARGET_CMDS
+	install -m 0755 -D $(@D)/ax-agent $(TARGET_DIR)/usr/sbin/ax-agent
+endef
+
+$(eval $(generic-package))
