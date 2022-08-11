fa23134a92c0dad89e0b74ecba6f93f6f65a135c
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-08-11 20:00:08 +0300

    Fix typo in packages

--- general/package/ax-agent/ax-agent.mk
+++ general/package/ax-agent/ax-agent.mk
@@ -8,15 +8,15 @@ AX_AGENT_LICENSE = MIT
 AX_AGENT_LICENSE_FILES = LICENSE
 
 define AX_AGENT_EXTRACT_CMDS
-	cp ../general/package/$(PKG_NAME)/src/$(PKG_NAME).c $(@D)/
+	cp ../general/package/ax-agent/src/ax-agent.c $(@D)/
 endef
 
 define AX_AGENT_BUILD_CMDS
-	(cd $(@D); $(TARGET_CC) -s $(PKG_NAME).c -o ax-agent)
+	(cd $(@D); $(TARGET_CC) -s ax-agent.c -o ax-agent)
 endef
 
 define AX_AGENT_INSTALL_TARGET_CMDS
-	install -m 0755 -D $(@D)/$(PKG_NAME) $(TARGET_DIR)/usr/sbin/$(PKG_NAME)
+	install -m 0755 -D $(@D)/ax-agent $(TARGET_DIR)/usr/sbin/ax-agent
 endef
 
 $(eval $(generic-package))
--- general/package/i2c-telemetry/i2c-telemetry.mk
+++ general/package/i2c-telemetry/i2c-telemetry.mk
@@ -8,7 +8,7 @@ I2C_TELEMETRY_LICENSE = MIT
 I2C_TELEMETRY_LICENSE_FILES = LICENSE
 
 define I2C_TELEMETRY_EXTRACT_CMDS
-	cp -av ../general/package/$(PKG_NAME)/src/* $(@D)/
+	cp -av ../general/package/i2c-telemetry/src/* $(@D)/
 endef
 
 define I2C_TELEMETRY_BUILD_CMDS
@@ -16,7 +16,7 @@ define I2C_TELEMETRY_BUILD_CMDS
 endef
 
 define I2C_TELEMETRY_INSTALL_TARGET_CMDS
-	install -m 0755 -D $(@D)/ina219.c $(TARGET_DIR)/usr/sbin/ina219.c
+	install -m 0755 -D $(@D)/ina219 $(TARGET_DIR)/usr/sbin/ina219
 endef
 
 $(eval $(generic-package))
