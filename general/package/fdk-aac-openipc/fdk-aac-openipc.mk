76a7f4f17c769fc88a052e11bc661dfea2fee150
Dmitry Ilyin <dima@doty.ru>
2022-08-18 22:19:03 +0300

    Tinify libfdk encoder

--- general/package/fdk-aac-openipc/fdk-aac-openipc.mk
+++ general/package/fdk-aac-openipc/fdk-aac-openipc.mk
@@ -4,20 +4,22 @@
 #
 ################################################################################
 
-FDK_AAC_OPENIPC_VERSION = 2.0.2
-FDK_AAC_OPENIPC_SITE = $(call github,mstorsjo,fdk-aac,v$(FDK_AAC_OPENIPC_VERSION))
+FDK_AAC_OPENIPC_VERSION = tinification
+FDK_AAC_OPENIPC_SITE = $(call github,widgetii,fdk-aac,$(FDK_AAC_OPENIPC_VERSION))
 FDK_AAC_OPENIPC_LICENSE = fdk-aac license
 FDK_AAC_OPENIPC_LICENSE_FILES = NOTICE
 FDK_AAC_OPENIPC_INSTALL_STAGING = YES
-FDK_AAC_OPENIPC_CONF_OPTS = --disable-static
+FDK_AAC_OPENIPC_CONF_OPTS = \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE \
+	-DCMAKE_POLICY_DEFAULT_CMP0069=NEW \
+	-DDISABLE_SBR_ENCODER=ON \
+	-DDISABLE_SAC_ENCODER=ON \
+	-DDISABLE_META_ENCODER=ON \
+	-DDISABLE_NOISE_SHAPING=ON \
+	-DDISABLE_TRANSPORT_ENCODER=ON \
+	-DDISABLE_STEREO=ON \
+	-DDISABLE_DECODERS=ON \
+	-DCMAKE_BUILD_TYPE=Release
 
-# This package uses autoconf, but not automake, so we need to call
-# their special autogen.sh script, and have custom target and staging
-# installation commands.
-
-define FDK_AAC_OPENIPC_RUN_AUTOGEN
-	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
-endef
-FDK_AAC_OPENIPC_PRE_CONFIGURE_HOOKS += FDK_AAC_OPENIPC_RUN_AUTOGEN
-
-$(eval $(autotools-package))
+$(eval $(cmake-package))
