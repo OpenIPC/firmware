ff2946634b3889cce47b066613ce05c7a912adfe
Dmitry Ilyin <dima@doty.ru>
2022-08-19 12:27:41 +0300

    Improve majestic.mk configuration

--- general/package/majestic/majestic.mk
+++ general/package/majestic/majestic.mk
@@ -5,38 +5,39 @@
 ################################################################################
 
 MAJESTIC_VERSION = current
-MAJESTIC_SOURCE =
 MAJESTIC_SITE = https://openipc.s3-eu-west-1.amazonaws.com
-MAJESTIC_LICENSE = MIT
+MAJESTIC_LICENSE = PROPRIETARY
 MAJESTIC_LICENSE_FILES = LICENSE
 
 FAMILY := $(shell grep "/board/" $(BR2_CONFIG) | head -1 | cut -d "/" -f 3)
 
-ifeq ($(BR2_PACKAGE_MAJESTIC_ULTIMATE),y)
-BUILD = ultimate
-else ifeq ($(BR2_PACKAGE_MAJESTIC_FPV),y)
-BUILD = fpv
-else
-BUILD = lite
+# TODO: change field number removing unknown_unknown
+RELEASE := $(shell grep "BR2_DEFCONFIG" $(BR2_CONFIG) | head -1 | cut -d "_" -f 5)
+
+ifneq ($(RELEASE),ultimate)
+	RELEASE := lite
 endif
 
-ifeq ($(BR2_PACKAGE_HISILICON_OSDRV_HI3516EV300),y)
-MAJESTIC_SOURCE = majestic.hi3516ev300.$(BUILD).master.tar.bz2
-else ifeq ($(BR2_PACKAGE_INGENIC_OSDRV_T20),y)
-MAJESTIC_SOURCE = majestic.t21.$(BUILD).master.tar.bz2
-else ifeq ($(BR2_PACKAGE_INGENIC_OSDRV_T21),y)
-MAJESTIC_SOURCE = majestic.t21.$(BUILD).master.tar.bz2
+ifeq ($(BR2_PACKAGE_INGENIC_OSDRV_T20),y)
+	FAMILY := t21
 else ifeq ($(BR2_PACKAGE_INGENIC_OSDRV_T30),y)
-MAJESTIC_SOURCE = majestic.t30.$(BUILD).master.tar.bz2
+	FAMILY := t31
 else ifeq ($(BR2_PACKAGE_INGENIC_OSDRV_T31),y)
-MAJESTIC_SOURCE = majestic.t31.$(BUILD).master.tar.bz2
-else ifeq ($(BR2_PACKAGE_SIGMASTAR_OSDRV_MSC313E),y)
-MAJESTIC_SOURCE = majestic.msc313e.$(BUILD).master.tar.bz2
+	FAMILY := t31
 else ifeq ($(BR2_PACKAGE_SIGMASTAR_OSDRV_SSC335),y)
-MAJESTIC_SOURCE = majestic.ssc335.$(BUILD).master.tar.bz2
-else
-MAJESTIC_SOURCE = majestic.$(FAMILY).$(BUILD).master.tar.bz2
+	FAMILY := ssc335
 endif
+
+MAJESTIC_SOURCE := majestic.$(FAMILY).$(RELEASE).master.tar.bz2
+
+MAJESTIC_DEPENDENCIES = \
+	libevent-openipc \
+	mbedtls-openipc \
+	json-c-openipc \
+	zlib \
+	lame-openipc \
+	opus-openipc \
+	libogg-openipc
 
 define MAJESTIC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
