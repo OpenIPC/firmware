0365e419a924ccc0961fa93d734a1d885141f897
Dmitry Ilyin <dima@doty.ru>
2022-08-19 18:15:00 +0300

    Add AWS WebRTC libraries and deps (#383)

---
+++ general/package/aws-webrtc/aws-webrtc.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# aws-webrtc
+#
+################################################################################
+
+AWS_WEBRTC_VERSION = v1.7.3
+AWS_WEBRTC_SITE_METHOD = git
+AWS_WEBRTC_SITE = https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-c
+AWS_WEBRTC_INSTALL_STAGING = YES
+AWS_WEBRTC_LICENSE = Apache-2.0
+AWS_WEBRTC_LICENSE_FILES = LICENSE
+
+AWS_WEBRTC_CONF_OPTS += \
+	-DBUILD_STATIC_LIBS=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DUSE_OPENSSL=OFF \
+	-DUSE_MBEDTLS=ON \
+	-DENABLE_DATA_CHANNEL=OFF \
+	-DBUILD_DEPENDENCIES=OFF
+
+AWS_WEBRTC_MAKE_OPTS += VERBOSE=1
+
+AWS_WEBRTC_DEPENDENCIES = \
+	libsrtp \
+	libwebsockets-openipc \
+	aws-producer \
+	usrsctp \
+	mbedtls-openipc
+
+$(eval $(cmake-package))
