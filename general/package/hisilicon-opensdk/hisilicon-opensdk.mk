34b7570cc101747241fb00cbba9147af67c1bcf3
Dmitry Ilyin <dima@doty.ru>
2022-08-23 12:10:57 +0300

    Test on gk7605v100

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HISILICON_OPENSDK_VERSION = a768b49b7540cb564891c3577071446acb8ac7c3
+HISILICON_OPENSDK_VERSION = 96dc0a811d3b6f119b7ad6eca57ddaa8e20d8dd9
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
