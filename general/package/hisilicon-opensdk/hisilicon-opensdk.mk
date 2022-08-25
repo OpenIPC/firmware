547cf7e14378ae5cf44ba6f9becc03fb8f72ca48
Dmitry Ilyin <dima@doty.ru>
2022-08-25 14:12:13 +0300

    Bump hisilicon-opensdk

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HISILICON_OPENSDK_VERSION = 93908316210d5e359658d16bfb529c8b99cf4fed
+HISILICON_OPENSDK_VERSION = 3094c2a8933abe5fc9ecc62b388ad2fbdd34724f
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
@@ -13,7 +13,7 @@ FAMILY = $(shell grep "/board/" $(BR2_CONFIG) | head -1 | cut -d "/" -f 3)
 
 HISILICON_OPENSDK_MODULE_SUBDIRS = kernel
 HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
-	CHIPSET=$(FAMILY)
+	CHIPARCH=$(FAMILY)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
