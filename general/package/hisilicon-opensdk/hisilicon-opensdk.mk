a436c2b3dc02a84db2f7320aaa6125654f21caae
Dmitry Ilyin <dima@doty.ru>
2022-08-25 15:36:12 +0300

    Remove all vendor binary modules except for isp (need more tests)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -13,6 +13,10 @@ FAMILY = $(shell grep "/board/" $(BR2_CONFIG) | head -1 | cut -d "/" -f 3)
 
 HISILICON_OPENSDK_MODULE_SUBDIRS = kernel
 HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
+	DISABLE_IST=1 \
+	DISABLE_PM=1 \
+	DISABLE_TDE=1 \
+	DISABLE_VO=1 \
 	CHIPARCH=$(FAMILY)
 
 $(eval $(kernel-module))
