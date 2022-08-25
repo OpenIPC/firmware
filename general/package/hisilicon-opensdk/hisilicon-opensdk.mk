79fcff1be0535f1b6db39e1f5ba6f7d146dd025b
Dmitry Ilyin <dima@doty.ru>
2022-08-25 16:07:48 +0300

    Disable ISP ko build

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HISILICON_OPENSDK_VERSION = a436c2b3dc02a84db2f7320aaa6125654f21caae
+HISILICON_OPENSDK_VERSION = e8701ddf4f89f0d4a523ae92c4dc712aba356240
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
@@ -17,6 +17,7 @@ HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
 	DISABLE_PM=1 \
 	DISABLE_TDE=1 \
 	DISABLE_VO=1 \
+	DISABLE_ISP=1 \
 	CHIPARCH=$(FAMILY)
 
 $(eval $(kernel-module))
