da3621beebc76c394063bea85b3be987480ec292
Dmitry Ilyin <dima@doty.ru>
2022-09-08 19:54:39 +0300

    Simplify external.mk

--- br-ext-chip-ambarella/external.mk
+++ br-ext-chip-ambarella/external.mk
@@ -1,28 +1,2 @@
 include $(BR2_EXTERNAL_AMBARELLA_PATH)/linux/linux-ext-ambarella_patcher.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/ambarella-osdrv-s3l/ambarella-osdrv-s3l.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/ambarella_patcher/ambarella_patcher.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/aura-httpd/aura-httpd.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/dropbear-openipc/dropbear-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/fdk-aac-openipc/fdk-aac-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/fwprintenv-openipc/fwprintenv-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/ipctool/ipctool.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/json-c-openipc/json-c-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/lame-openipc/lame-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libcurl-openipc/libcurl-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libevent-openipc/libevent-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libhv-openipc/libhv-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libogg-openipc/libogg-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libsrt-openipc/libsrt-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/libwebsockets-openipc/libwebsockets-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/linux-firmware-openipc/linux-firmware-openipc.mk
-# include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/majestic/majestic.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/majestic-fonts/majestic-fonts.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/mbedtls-openipc/mbedtls-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/microbe-web/microbe-web.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/motors/motors.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/node-exporter/node-exporter.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/opus-openipc/opus-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/rtl8188fu-openipc/rtl8188fu-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/uacme-openipc/uacme-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/vtund-openipc/vtund-openipc.mk
-include $(BR2_EXTERNAL_AMBARELLA_PATH)/package/yaml-cli/yaml-cli.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_AMBARELLA_PATH)/package/*/*.mk))
--- br-ext-chip-anyka/external.mk
+++ br-ext-chip-anyka/external.mk
@@ -1,28 +1,2 @@
 include $(BR2_EXTERNAL_ANYKA_PATH)/linux/linux-ext-anyka_patcher.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/anyka-osdrv-ak3918ev200/anyka-osdrv-ak3918ev200.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/anyka_patcher/anyka_patcher.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/aura-httpd/aura-httpd.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/dropbear-openipc/dropbear-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/fdk-aac-openipc/fdk-aac-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/fwprintenv-openipc/fwprintenv-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/ipctool/ipctool.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/json-c-openipc/json-c-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/lame-openipc/lame-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libcurl-openipc/libcurl-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libevent-openipc/libevent-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libhv-openipc/libhv-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libogg-openipc/libogg-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libsrt-openipc/libsrt-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/libwebsockets-openipc/libwebsockets-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/linux-firmware-openipc/linux-firmware-openipc.mk
-# include $(BR2_EXTERNAL_ANYKA_PATH)/package/majestic/majestic.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/majestic-fonts/majestic-fonts.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/mbedtls-openipc/mbedtls-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/microbe-web/microbe-web.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/motors/motors.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/node-exporter/node-exporter.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/opus-openipc/opus-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/rtl8188fu-openipc/rtl8188fu-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/uacme-openipc/uacme-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/vtund-openipc/vtund-openipc.mk
-include $(BR2_EXTERNAL_ANYKA_PATH)/package/yaml-cli/yaml-cli.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_ANYKA_PATH)/package/*/*.mk))
