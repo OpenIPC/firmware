5b153759ccb11eb721720fea245274571732ce71
Dmitry Ilyin <dima@doty.ru>
2022-09-08 22:53:30 +0300

    Simplify external.mk (#446)

--- br-ext-chip-rockchip/external.mk
+++ br-ext-chip-rockchip/external.mk
@@ -1,28 +1,2 @@
 include $(BR2_EXTERNAL_ROCKCHIP_PATH)/linux/linux-ext-rockchip_patcher.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/rockchip_patcher/rockchip_patcher.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/aura-httpd/aura-httpd.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/dropbear-openipc/dropbear-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/fdk-aac-openipc/fdk-aac-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/fwprintenv-openipc/fwprintenv-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/ipctool/ipctool.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/json-c-openipc/json-c-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/lame-openipc/lame-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libcurl-openipc/libcurl-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libevent-openipc/libevent-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libhv-openipc/libhv-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libogg-openipc/libogg-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libsrt-openipc/libsrt-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/libwebsockets-openipc/libwebsockets-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/linux-firmware-openipc/linux-firmware-openipc.mk
-# include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/majestic/majestic.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/majestic-fonts/majestic-fonts.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/mbedtls-openipc/mbedtls-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/microbe-web/microbe-web.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/motors/motors.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/node-exporter/node-exporter.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/rockchip-osdrv-rv11xx/rockchip-osdrv-rv11xx.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/opus-openipc/opus-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/rtl8188fu-openipc/rtl8188fu-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/uacme-openipc/uacme-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/vtund-openipc/vtund-openipc.mk
-include $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/yaml-cli/yaml-cli.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_ROCKCHIP_PATH)/package/*/*.mk))
