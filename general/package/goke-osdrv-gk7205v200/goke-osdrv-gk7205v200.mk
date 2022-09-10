eab3665b8c7c8b0cc411310809c34b6723c72503
Igor Zalatov <flyrouter@gmail.com>
2022-09-11 00:18:08 +0300

    Set IMX307 IQ profile as default, Remove old an unused configs for hi3518ev200, Add sysreset.sh script for resen network or majestic configs, Change notify in sysupgrade (#449)

--- general/package/goke-osdrv-gk7205v200/goke-osdrv-gk7205v200.mk
+++ general/package/goke-osdrv-gk7205v200/goke-osdrv-gk7205v200.mk
@@ -38,7 +38,7 @@ define GOKE_OSDRV_GK7205V200_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iq $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/iq/imx307.ini
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iq $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/iq/imx335.ini
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iq $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/sensor/iq/sc2232.ini
-	ln -sf imx335.ini $(TARGET_DIR)/etc/sensors/iq/default.ini
+	ln -sf imx307.ini $(TARGET_DIR)/etc/sensors/iq/default.ini
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/goke
 	## $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/goke $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk7205v200/files/kmod/cipher_drv.ko
