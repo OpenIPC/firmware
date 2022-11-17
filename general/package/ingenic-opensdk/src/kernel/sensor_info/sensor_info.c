9d675029ac1ce2d844ec30439b0031ac563fde3f
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-11-17 23:27:00 +0300

    Update sinfo kernel module

--- general/package/ingenic-opensdk/src/kernel/sensor_info/sensor_info.c
+++ general/package/ingenic-opensdk/src/kernel/sensor_info/sensor_info.c
@@ -171,6 +171,7 @@ enum SENSOR_TYPE
 	SENSOR_TYPE_SC500AI,
 	SENSOR_TYPE_JXQ03,
 	SENSOR_TYPE_JXQ03P,
+	SENSOR_TYPE_GC2083,
 };
 
 SENSOR_INFO_T g_sinfo[] =
@@ -257,6 +258,7 @@ SENSOR_INFO_T g_sinfo[] =
 	{"sc500ai", 0x30,  "cgu_cim", 24000000, {0xce, 0x1f}, 1, {0x3107, 0x3108}, 2, 2, NULL},
 	{"jxq03",  0x40,  "cgu_cim", 24000000, {0x05, 0x07}, 1, {0xa, 0xb}, 1, 2, NULL},
 	{"jxq03p",  0x40,  "cgu_cim", 24000000, {0x08, 0x43}, 1, {0xa, 0xb}, 1, 2, NULL},
+	{"gc2083", 0x37,  "cgu_cim", 24000000, {0x20, 0x83}, 1, {0xf0, 0xf1}, 1, 2, NULL},
 };
 
 static int8_t g_sensor_id = -1;
