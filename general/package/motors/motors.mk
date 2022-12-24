ce209d47e9b20a5d6761abcf65b616c9b5d5c81f
Igor Zalatov <flyrouter@gmail.com>
2022-12-24 21:52:49 +0300

    Add ms32006 motor driver support (#599)

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -1,22 +1,24 @@
 ################################################################################
 #
-# motors | updated 2021.11.06
+# motors | updated 2022.12.24
 #
 ################################################################################
 
-MOTORS_VERSION = 32ba1309e739c068a89132aaebcde69b71fdaf4a
+MOTORS_VERSION = d6a16ef7907c62381b24cd27a00cbf00e038f110
 MOTORS_SITE = $(call github,openipc,motors,$(MOTORS_VERSION))
 MOTORS_LICENSE = MIT
 MOTORS_LICENSE_FILES = LICENSE
 
 define MOTORS_BUILD_CMDS
 	(cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor)
+	(cd $(@D)/i2c-motor; $(TARGET_CC) -Os -s main.c -o i2c-motor)
 	(cd $(@D)/xm-kmotor; $(TARGET_CC) -Os -s main.c -o xm-kmotor)
 	(cd $(@D)/xm-uart; $(TARGET_CC) -Os -s main.c -o xm-uart)
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor
+	$(INSTALL) -m 0755 -D $(@D)/i2c-motor/i2c-motor $(TARGET_DIR)/usr/bin/i2c-motor
 	$(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor
 	$(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart
 endef
