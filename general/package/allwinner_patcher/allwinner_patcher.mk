b61c64acfe2a6ec470762ebbc825fdf635b45e14
cronyx <xcronyx@gmail.com>
2022-12-26 14:44:09 +0300

    add allwinner v83x initial support

---
+++ general/package/allwinner_patcher/allwinner_patcher.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# allwinner_patcher
+#
+################################################################################
+
+ALLWINNER_PATCHER_VERSION = 0.1
+ALLWINNER_PATCHER_INSTALL_TARGET = NO
+ALLWINNER_PATCHER_SITE =
+ALLWINNER_PATCHER_SOURCE =
+
+$(eval $(generic-package))
