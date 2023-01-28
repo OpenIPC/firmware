bb080810e417299ac8c4f9370eaa4acebf2c5ec1
Dmitry Ermakov <de@dimer.org.ua>
2023-01-28 23:43:38 +0300

    [GK710x] Fix osdrv makefile

--- general/package/goke-osdrv-gk710x/goke-osdrv-gk710x.mk
+++ general/package/goke-osdrv-gk710x/goke-osdrv-gk710x.mk
@@ -9,6 +9,14 @@ GOKE_OSDRV_GK710X_SITE =
 GOKE_OSDRV_GK710X_LICENSE = MIT
 GOKE_OSDRV_GK710X_LICENSE_FILES = LICENSE
 
+CHIP := $(shell echo $(BOARD) | cut -d "_" -f 1)
+
+ifeq ($(CHIP),gk7102)
+	FIRMWARE=gk_fw_710x.bin
+else ifeq ($(CHIP),gk7102s)
+	FIRMWARE=gk_fw_710xs.bin
+endif
+
 define GOKE_OSDRV_GK710X_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/files/script/S95goke
@@ -18,7 +26,7 @@ define GOKE_OSDRV_GK710X_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/files/sensor/config/sc1135.bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/firmware $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/files/sensor/fw/*.bin
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/firmware $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/files/sensor/fw/$(FIRMWARE)
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.4.43-gk/goke
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.4.43-gk/goke $(BR2_EXTERNAL_GOKE_PATH)/package/goke-osdrv-gk710x/files/kmod/*.ko
