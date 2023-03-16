993cb08625106cffad1b92b6be3640785f12d666
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-03-16 13:37:20 +0100

    [Sigmastar] Add simple initramfs that boots rootfs from sdcard (#671)
    Co-authored-by: Viktor <viktorxda@users.noreply.github.com>

---
+++ general/package/sigmastar-initramfs/sigmastar-initramfs.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# sigmastar-initramfs
+#
+################################################################################
+
+SIGMASTAR_INITRAMFS_VERSION = 1.0
+SIGMASTAR_INITRAMFS_SITE =
+SIGMASTAR_INITRAMFS_SOURCE =
+
+$(eval $(generic-package))
