f9019ecb8a1fced582d2959cd5c8d85f192b980b
Viktor <viktorxda@users.noreply.github.com>
2023-03-18 19:39:22 +0100

    Move sdcard scripts to proper place

--- general/package/wireless-configuration/wireless-configuration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILES=../general/package/wireless-configuration/files
-cp -f $FILES/udhcpd.conf $TARGET_DIR/etc
-cp -f $FILES/interfaces $TARGET_DIR/etc/network
-cp -f $FILES/netadapter $TARGET_DIR/etc/network
-cp -f $FILES/netsetup $TARGET_DIR/etc/network
-cp -f $FILES/wireless $TARGET_DIR/usr/sbin
