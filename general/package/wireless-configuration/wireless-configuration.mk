6b31fca4c76c6fe233fb2d900cae7b56e2c0608b
Viktor <viktorxda@users.noreply.github.com>
2023-03-26 19:05:28 +0200

    [Sigmastar] update infinity6e scripts

--- general/package/wireless-configuration/wireless-configuration.mk
+++ general/package/wireless-configuration/wireless-configuration.mk
@@ -9,19 +9,24 @@ WIRELESS_CONFIGURATION_SITE =
 WIRELESS_CONFIGURATION_LICENSE = MIT
 WIRELESS_CONFIGURATION_LICENSE_FILES = LICENSE
 
-WIRELESS_CONFIGURATION_PATH = ../general/package/wireless-configuration/files
+WIRELESS_CONFIGURATION_PATH = $(WIRELESS_CONFIGURATION_PKGDIR)/files
 
 define WIRELESS_CONFIGURATION_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WIRELESS_CONFIGURATION_PATH)/udhcpd.conf
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(WIRELESS_CONFIGURATION_PATH)/network/udhcpd.conf
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/wireless
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/wireless $(WIRELESS_CONFIGURATION_PATH)/interfaces
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/wireless $(WIRELESS_CONFIGURATION_PATH)/netadapter
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/wireless $(WIRELESS_CONFIGURATION_PATH)/netsetup
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIRELESS_CONFIGURATION_PATH)/script/S30wireless
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/network
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network $(WIRELESS_CONFIGURATION_PATH)/network/interfaces.conf
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/network $(WIRELESS_CONFIGURATION_PATH)/script/adapter
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIRELESS_CONFIGURATION_PATH)/wireless
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIRELESS_CONFIGURATION_PATH)/script/wireless
+
+	# Set config for wireless adapter
+	sed -i "s/CONFIG=/CONFIG=$(notdir $(DEFCONFIG))/g" $(TARGET_DIR)/etc/network/adapter
 endef
 
 $(eval $(generic-package))
