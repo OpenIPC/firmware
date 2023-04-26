9b22373a8b0b1ffd98f40443722abe4723d233f2
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-04-26 11:32:34 +0200

    [Sigmastar] update infinity6e sensor handling (#730)
    Co-authored-by: Viktor <viktorxda@users.noreply.github.com>

--- general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
+++ general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
@@ -10,7 +10,6 @@ SIGMASTAR_OSDRV_INFINITY6E_LICENSE = MIT
 SIGMASTAR_OSDRV_INFINITY6E_LICENSE_FILES = LICENSE
 
 SIGMASTAR_OSDRV_INFINITY6E_PATH = $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files
-SIGMASTAR_OSDRV_INFINITY6E_SENSOR = $(call qstrip,$(BR2_PACKAGE_SIGMASTAR_OSDRV_SENSOR))
 
 define SIGMASTAR_OSDRV_INFINITY6E_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
@@ -21,17 +20,13 @@ define SIGMASTAR_OSDRV_INFINITY6E_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.84/sigmastar
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/kmod/*.ko
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/sensor/$(SIGMASTAR_OSDRV_INFINITY6E_SENSOR)/*.ko
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/sensor/kmod/*.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/iqfile
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iqfile $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/sensor/$(SIGMASTAR_OSDRV_INFINITY6E_SENSOR)/$(SIGMASTAR_OSDRV_INFINITY6E_SENSOR)_iqfile.bin
-	ln -sf $(SIGMASTAR_OSDRV_INFINITY6E_SENSOR)_iqfile.bin $(TARGET_DIR)/etc/sensors/iqfile/iqfile0.bin
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iqfile $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/sensor/iqfile/iqfile0.bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/venc_fw
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/venc_fw $(SIGMASTAR_OSDRV_INFINITY6E_PATH)/sensor/venc_fw/chagall.bin
-
-	# Set sensor for insmod
-	sed -i "s/sensor=/sensor=$(SIGMASTAR_OSDRV_INFINITY6E_SENSOR)/g" $(TARGET_DIR)/usr/bin/load_sigmastar
 endef
 
 $(eval $(generic-package))
