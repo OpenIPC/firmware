ae0166c57a9a838c14940d5618e7c434db2d8726
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-07 14:55:37 +0200

    [General] export external vendor (#767)
    Co-authored-by: viktorxda <viktorxda@users.noreply.github.com>

--- general/external.mk
+++ general/external.mk
@@ -1,6 +1,6 @@
 include $(BR2_EXTERNAL_GENERAL_PATH)/linux/linux-ext-vendor-patcher.mk
 include $(sort $(wildcard $(BR2_EXTERNAL_GENERAL_PATH)/package/*/*.mk))
 
-EXTERNAL_VENDOR = $(shell echo $(BR2_DEFCONFIG) | grep -o "br-ext-chip-\w*")
+export EXTERNAL_VENDOR = $(shell echo $(BR2_DEFCONFIG) | grep -o "br-ext-chip-\w*")
 BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../$(EXTERNAL_VENDOR)
 BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../scripts
