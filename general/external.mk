ca3fffe5cde9de70cc7243409037870226cb3eb1
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-08 04:05:54 +0200

    [General] use global release and family variable (#768)
    Co-authored-by: viktorxda <viktorxda@users.noreply.github.com>

--- general/external.mk
+++ general/external.mk
@@ -1,6 +1,9 @@
+export EXTERNAL_FAMILY = $(shell cat $(BR2_CONFIG) | grep -oE -m1 "board.\w+" | cut -d "/" -f 2)
+export EXTERNAL_RELEASE = $(shell cat $(BR2_CONFIG) | grep -oE "\w+_defconfig" | cut -d "_" -f 2)
+export EXTERNAL_VENDOR = $(shell cat $(BR2_CONFIG) | grep -oE "br-ext-chip-\w+" | cut -d "-" -f 4)
+
+BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../br-ext-chip-$(EXTERNAL_VENDOR)
+BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../scripts
+
 include $(BR2_EXTERNAL_GENERAL_PATH)/linux/linux-ext-vendor-patcher.mk
 include $(sort $(wildcard $(BR2_EXTERNAL_GENERAL_PATH)/package/*/*.mk))
-
-export EXTERNAL_VENDOR = $(shell echo $(BR2_DEFCONFIG) | grep -o "br-ext-chip-\w*")
-BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../$(EXTERNAL_VENDOR)
-BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../scripts
