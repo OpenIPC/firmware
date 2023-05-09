b54e7bec7a270110f4116b41870551cb4bc821dd
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-09 14:36:19 +0200

    [General] centralize kernel patcher list (#775)
    Co-authored-by: Viktor <viktorxda@users.noreply.github.com>

--- general/external.mk
+++ general/external.mk
@@ -1,6 +1,6 @@
-export EXTERNAL_FAMILY = $(shell cat $(BR2_CONFIG) | grep -oE -m1 "board.\w+" | cut -d "/" -f 2)
-export EXTERNAL_RELEASE = $(shell cat $(BR2_CONFIG) | grep -oE "\w+_defconfig" | cut -d "_" -f 2)
-export EXTERNAL_VENDOR = $(shell cat $(BR2_CONFIG) | grep -oE "br-ext-chip-\w+" | cut -d "-" -f 4)
+EXTERNAL_FAMILY = $(shell cat $(BR2_CONFIG) | grep -oE "board.\w+" | cut -d "/" -f 2)
+EXTERNAL_RELEASE = $(shell cat $(BR2_CONFIG) | grep -oE "\w+_defconfig" | cut -d "_" -f 2)
+EXTERNAL_VENDOR = $(shell cat $(BR2_CONFIG) | grep -oE "br-ext-chip-\w+" | cut -d "-" -f 4)
 
 BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../br-ext-chip-$(EXTERNAL_VENDOR)
 BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../scripts
