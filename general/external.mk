e7816147fd9bb34d30bd7931c7439aa9cd7d118f
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-12 12:49:01 +0200

    [General] move scripts and patches to general folder (#782)
    Co-authored-by: Viktor <viktorxda@users.noreply.github.com>

--- general/external.mk
+++ general/external.mk
@@ -1,9 +1,9 @@
-EXTERNAL_FAMILY = $(shell cat $(BR2_CONFIG) | grep -oE "board.\w+" | cut -d "/" -f 2)
-EXTERNAL_RELEASE = $(shell cat $(BR2_CONFIG) | grep -oE "\w+_defconfig" | cut -d "_" -f 2)
-EXTERNAL_VENDOR = $(shell cat $(BR2_CONFIG) | grep -oE "br-ext-chip-\w+" | cut -d "-" -f 4)
+EXTERNAL_FAMILY = $(shell grep -oE "board.\w+" $(BR2_CONFIG) | cut -d "/" -f 2)
+EXTERNAL_RELEASE = $(shell grep -oE "\w+_defconfig" $(BR2_CONFIG) | cut -d "_" -f 2)
+EXTERNAL_VENDOR = $(shell grep -oE "br-ext-chip-\w+" $(BR2_CONFIG))
 
-BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../br-ext-chip-$(EXTERNAL_VENDOR)
-BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../scripts
+export BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../$(EXTERNAL_VENDOR)
+export BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/scripts
 
 include $(BR2_EXTERNAL_GENERAL_PATH)/linux/linux-ext-vendor-patcher.mk
 include $(sort $(wildcard $(BR2_EXTERNAL_GENERAL_PATH)/package/*/*.mk))
