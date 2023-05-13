98935b67fecdf653a75ebe569e6e873261580f56
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2023-05-13 18:31:58 +0300

    Fix compile uqmi and cosmetic changes baresip

--- general/package/libre-openipc/libre-openipc.mk
+++ general/package/libre-openipc/libre-openipc.mk
@@ -18,20 +18,20 @@ endif
 
 define LIBRE_OPENIPC_BUILD_CMDS
     $(TARGET_MAKE_ENV) \
-	$(MAKE) -C $(@D) \
-	    LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
-	    LIBRE_INC=$(STAGING_DIR)/usr/include/re \
-	    LIBRE_SO=$(STAGING_DIR)/usr/lib \
-	    CC="$(TARGET_CC)" \
-	    EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
-	    EXTRA_LFLAGS="-lm" \
-	    DESTDIR="$(STAGING_DIR)" \
-	    SYSROOT="$(STAGING_DIR)/usr" \
-	    SYSROOT_ALT="$(STAGING_DIR)/usr" \
-	    RELEASE=1 \
-	    CROSS_COMPILE="$(TARGET_CROSS)" \
-	    OS=linux \
-	    all install
+    $(MAKE) -C $(@D) \
+        LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
+        LIBRE_INC=$(STAGING_DIR)/usr/include/re \
+        LIBRE_SO=$(STAGING_DIR)/usr/lib \
+        CC="$(TARGET_CC)" \
+        EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
+        EXTRA_LFLAGS="-lm" \
+        DESTDIR="$(STAGING_DIR)" \
+        SYSROOT="$(STAGING_DIR)/usr" \
+        SYSROOT_ALT="$(STAGING_DIR)/usr" \
+        RELEASE=1 \
+        CROSS_COMPILE="$(TARGET_CROSS)" \
+        OS=linux \
+        all install
 endef
 
 
