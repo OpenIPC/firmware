41d68968e33ff2610f73d4fc6840206ce72855f3
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2023-05-15 01:36:00 +0300

    Fix baresip

--- general/package/libre-openipc/libre-openipc.mk
+++ general/package/libre-openipc/libre-openipc.mk
@@ -17,36 +17,40 @@ LIBRE_OPENIPC_DEPENDENCIES += zlib
 endif
 
 define LIBRE_OPENIPC_BUILD_CMDS
-    $(TARGET_MAKE_ENV) \
-    $(MAKE) -C $(@D) \
-        LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
-        LIBRE_INC=$(STAGING_DIR)/usr/include/re \
-        LIBRE_SO=$(STAGING_DIR)/usr/lib \
-        CC="$(TARGET_CC)" \
-        EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
-        EXTRA_LFLAGS="-lm" \
-        DESTDIR="$(STAGING_DIR)" \
-        SYSROOT="$(STAGING_DIR)/usr" \
-        SYSROOT_ALT="$(STAGING_DIR)/usr" \
-        RELEASE=1 \
-        CROSS_COMPILE="$(TARGET_CROSS)" \
-        OS=linux \
-        all install
+$(TARGET_MAKE_ENV) \
+	$(MAKE) -C $(@D) \
+		LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
+		LIBRE_INC=$(STAGING_DIR)/usr/include/re \
+		LIBRE_SO=$(STAGING_DIR)/usr/lib \
+		CC="$(TARGET_CC)" \
+		EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
+		EXTRA_LFLAGS="-lm" \
+		DESTDIR="$(STAGING_DIR)" \
+		SYSROOT="$(STAGING_DIR)/usr" \
+		SYSROOT_ALT="$(STAGING_DIR)/usr" \
+		RELEASE=1 \
+		CROSS_COMPILE="$(TARGET_CROSS)" \
+		OS=linux \
+		all install
 endef
 
+define LIBRE_OPENIPC_INSTALL_STAGING_CMDS
+		$(INSTALL) -D -m 644 $(@D)/include/re.h $(STAGING_DIR)/usr/include/re/re.h
+endef
 
 define LIBRE_OPENIPC_INSTALL_TARGET_CMDS
-        $(INSTALL) -m 644 -D $(@D)/libre.so $(TARGET_DIR)/usr/lib/libre.so
+		$(INSTALL) -m 644 -D $(@D)/libre.so $(TARGET_DIR)/usr/lib/libre.so
+		ln -sf libre.so $(TARGET_DIR)/usr/lib/libre.so.2
 endef
 
 define LIBRE_OPENIPC_UNINSTALL_STAGING_CMDS
-        $(RM) -r $(STAGING_DIR)/usr/include/re
-        $(RM) $(STAGING_DIR)/usr/lib/libre.a
-        $(RM) $(STAGING_DIR)/usr/lib/libre.so
+		$(RM) -r $(STAGING_DIR)/usr/include/re
+		$(RM) $(STAGING_DIR)/usr/lib/libre.a
+		$(RM) $(STAGING_DIR)/usr/lib/libre.so
 endef
 
 define LIBRE_OPENIPC_UNINSTALL_TARGET_CMDS
-        $(RM) $(TARGET_DIR)/usr/lib/libre.so
+		$(RM) $(TARGET_DIR)/usr/lib/libre.so
 endef
 
 $(eval $(generic-package))
--- general/package/librem-openipc/librem-openipc.mk
+++ general/package/librem-openipc/librem-openipc.mk
@@ -14,37 +14,37 @@ LIBREM_OPENIPC_DEPENDENCIES += libre-openipc
 endif
 
 define LIBREM_OPENIPC_BUILD_CMDS
-    $(TARGET_MAKE_ENV) \
-    $(MAKE) -C $(@D) \
-        LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
-        LIBRE_INC=$(STAGING_DIR)/usr/include/re \
-        LIBRE_SO=$(STAGING_DIR)/usr/lib \
-        HAVE_LIBRESOLV= \
-        CC="$(TARGET_CC)" \
-        EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
-        EXTRA_LFLAGS="-lm" \
-        DESTDIR="$(STAGING_DIR)" \
-        SYSROOT="$(STAGING_DIR)/usr" \
-        SYSROOT_ALT="$(STAGING_DIR)/usr" \
-        RELEASE=1 \
-        CROSS_COMPILE="$(TARGET_CROSS)" \
-        OS=linux \
-        all install
+$(TARGET_MAKE_ENV) \
+	$(MAKE) -C $(@D) \
+		LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
+		LIBRE_INC=$(STAGING_DIR)/usr/include/re \
+		LIBRE_SO=$(STAGING_DIR)/usr/lib \
+		HAVE_LIBRESOLV= \
+		CC="$(TARGET_CC)" \
+		EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
+		EXTRA_LFLAGS="-lm" \
+		DESTDIR="$(STAGING_DIR)" \
+		SYSROOT="$(STAGING_DIR)/usr" \
+		SYSROOT_ALT="$(STAGING_DIR)/usr" \
+		RELEASE=1 \
+		CROSS_COMPILE="$(TARGET_CROSS)" \
+		OS=linux \
+		all install
 endef
 
 define LIBREM_OPENIPC_INSTALL_TARGET_CMDS
-        $(INSTALL) -m 644 -D $(@D)/librem.so $(TARGET_DIR)/usr/lib/librem.so
+		$(INSTALL) -m 644 -D $(@D)/librem.so $(TARGET_DIR)/usr/lib/librem.so
+		ln -sf librem.so $(TARGET_DIR)/usr/lib/librem.so.2
 endef
 
 define LIBREM_OPENIPC_UNINSTALL_STAGING_CMDS
-        $(RM) -r $(STAGING_DIR)/usr/include/rem
-        $(RM) $(STAGING_DIR)/usr/lib/librem.a
-        $(RM) $(STAGING_DIR)/usr/lib/librem.so
+		$(RM) -r $(STAGING_DIR)/usr/include/rem
+		$(RM) $(STAGING_DIR)/usr/lib/librem.a
+		$(RM) $(STAGING_DIR)/usr/lib/librem.so
 endef
 
 define LIBREM_OPENIPC_UNINSTALL_TARGET_CMDS
-        $(RM) $(TARGET_DIR)/usr/lib/librem.so
+		$(RM) $(TARGET_DIR)/usr/lib/librem.so
 endef
 
 $(eval $(generic-package))
-#$(eval $(host-generic-package))
