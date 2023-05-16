14c1bff3adf580b21ba4c0fbb3a1d6979d6ef712
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-05-16 18:03:41 +0200

    [General] simplify buildroot path names

--- general/linux/linux-ext-vendor-patcher.mk
+++ general/linux/linux-ext-vendor-patcher.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 LINUX_EXTENSIONS += vendor-patcher
-
-VENDOR_PATCHER_PATH = $(BR2_EXTERNAL_VENDOR_PATH)/board/$(EXTERNAL_FAMILY)/kernel
+VENDOR_PATCHER_PATH = $(BR2_EXTERNAL_VENDOR)/board/$(EXTERNAL_FAMILY)/kernel
 
 define VENDOR_PATCHER_PREPARE_KERNEL
 	$(VENDOR_PATCHER_PKGDIR)/apply.sh $(@D) "$(VENDOR_PATCHER_PATH)/patches/ $(VENDOR_PATCHER_PATH)/overlay"
