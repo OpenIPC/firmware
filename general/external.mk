6dcb2e2ea4a61fc2b5122043fc1c966910b3b264
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-05-16 18:03:41 +0200

    [General] simplify buildroot path names

--- general/external.mk
+++ general/external.mk
@@ -2,8 +2,8 @@ EXTERNAL_FAMILY = $(shell grep -oE "board.\w+" $(BR2_CONFIG) | cut -d "/" -f 2)
 EXTERNAL_RELEASE = $(shell grep -oE "\w+_defconfig" $(BR2_CONFIG) | cut -d "_" -f 2)
 EXTERNAL_VENDOR = $(shell grep -oE "br-ext-chip-\w+" $(BR2_CONFIG))
 
-export BR2_EXTERNAL_VENDOR_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/../$(EXTERNAL_VENDOR)
-export BR2_EXTERNAL_SCRIPTS_PATH = $(BR2_EXTERNAL_GENERAL_PATH)/scripts
+export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../$(EXTERNAL_VENDOR)
+export BR2_EXTERNAL_SCRIPTS = $(BR2_EXTERNAL)/scripts
 
-include $(BR2_EXTERNAL_GENERAL_PATH)/linux/linux-ext-vendor-patcher.mk
-include $(sort $(wildcard $(BR2_EXTERNAL_GENERAL_PATH)/package/*/*.mk))
+include $(BR2_EXTERNAL)/linux/linux-ext-vendor-patcher.mk
+include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
