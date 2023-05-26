67bd82351f891021bbd4e6bd1ffebb92c6881ec0
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-26 15:23:27 +0200

    General: set separate release name for lte devices

--- general/scripts/executing_commands.sh
+++ general/scripts/executing_commands.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-D=$(date +%y.%m.%d)
-R=${TARGET_DIR}/usr/lib/os-release
-echo OPENIPC_VERSION=${D:0:1}.${D:1} >> ${R}
-date +GITHUB_VERSION="\"${GIT_BRANCH}+${GIT_HASH}, %Y-%m-%d"\" >> ${R}
-date +TIME_STAMP=%s >> ${R}
-echo BUILD_OPTION=${OPENIPC_RELEASE} >> ${R}
+DATE=$(date +%y.%m.%d)
+FILE=${TARGET_DIR}/usr/lib/os-release
+
+echo OPENIPC_VERSION=${DATE:0:1}.${DATE:1} >> ${FILE}
+date +GITHUB_VERSION="\"${GIT_BRANCH-local}+${GIT_HASH-build}, %Y-%m-%d"\" >> ${FILE}
+echo BUILD_OPTION=${OPENIPC_RELEASE} >> ${FILE}
+date +TIME_STAMP=%s >> ${FILE}
 
 echo --- BR2_TOOLCHAIN_BUILDROOT_LIBC: ${BR2_EXTERNAL_LIBC}
 rm -f ${TARGET_DIR}/usr/bin/gdbserver
 
-if [ ${BR2_EXTERNAL_LIBC} != "glibc" ] && \
-  ! grep -q ^BR2_PACKAGE_WIFIBROADCAST=y ${BR2_CONFIG} && \
-  ! grep -q ^BR2_PACKAGE_LIBV4L=y ${BR2_CONFIG} && \
-  ! grep -q ^BR2_PACKAGE_MAVLINK_ROUTER=y ${BR2_CONFIG} && \
-  ! grep -q ^BR2_PACKAGE_INGENIC_OSDRV_T30=y ${BR2_CONFIG}; then
-    rm -f ${TARGET_DIR}/usr/lib/libstdc++*
+CONF="INGENIC_OSDRV_T30=y|LIBV4L=y|MAVLINK_ROUTER=y|WIFIBROADCAST=y"
+if [ ${BR2_EXTERNAL_LIBC} != "glibc" ] && ! grep -qE ${CONF} ${BR2_CONFIG}; then
+  rm -f ${TARGET_DIR}/usr/lib/libstdc++*
 fi
 
 if [ ${BR2_EXTERNAL_LIBC} = "musl" ]; then
-  exlist=${BR2_EXTERNAL_SCRIPTS}/excludes/${OPENIPC_MODEL}_${OPENIPC_RELEASE}.list
-  if [ -f ${exlist} ]; then
-    for e in $(cat ${exlist}); do
-      rm -f ${TARGET_DIR}${e}
-    done
-  fi
+  NAME=${OPENIPC_RELEASE/lte/fpv}
+  LIST=${BR2_EXTERNAL_SCRIPTS}/excludes/${OPENIPC_MODEL}_${NAME}.list
+  test -e ${LIST} && xargs -a ${LIST} -i rm -f ${TARGET_DIR}{}
 
   ln -sf /lib/libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
   ln -sf ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
