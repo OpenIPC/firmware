4f8ca43842cc25d4b55016112cb67bce5a4e50bc
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-26 22:09:12 +0200

    Workflow: create separate toolchain action

--- general/external.mk
+++ general/external.mk
@@ -3,7 +3,6 @@ export OPENIPC_MODEL = $(call qstrip,$(BR2_OPENIPC_MODEL))
 export OPENIPC_FAMILY = $(call qstrip,$(BR2_OPENIPC_FAMILY))
 export OPENIPC_RELEASE = $(call qstrip,$(BR2_OPENIPC_RELEASE))
 
-export BR2_EXTERNAL_LIBC ?= $(call qstrip,$(BR2_TOOLCHAIN_BUILDROOT_LIBC))
 export BR2_EXTERNAL_SCRIPTS = $(BR2_EXTERNAL)/scripts
 export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(BR2_OPENIPC_VENDOR)
 
