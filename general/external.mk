2c5c879f4a0ad831869ceb770daae5ac24209e0c
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-05-31 21:56:56 +0200

    Scripts: adjust post build script

--- general/external.mk
+++ general/external.mk
@@ -3,8 +3,7 @@ export OPENIPC_MODEL = $(call qstrip,$(BR2_OPENIPC_MODEL))
 export OPENIPC_FAMILY = $(call qstrip,$(BR2_OPENIPC_FAMILY))
 export OPENIPC_RELEASE = $(call qstrip,$(BR2_OPENIPC_RELEASE))
 
-export BR2_EXTERNAL_SCRIPTS = $(BR2_EXTERNAL)/scripts
 export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(BR2_OPENIPC_VENDOR)
-OPENIPC_TOOLCHAIN = latest/$(shell $(BR2_EXTERNAL_SCRIPTS)/show_toolchains.sh $(BR2_DEFCONFIG))
+OPENIPC_TOOLCHAIN = latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
--- general/scripts/rootfs_script.sh
+++ general/scripts/rootfs_script.sh
@@ -7,7 +7,6 @@ echo OPENIPC_VERSION=${DATE:0:1}.${DATE:1} >> ${FILE}
 date +GITHUB_VERSION="\"${GIT_BRANCH-local}+${GIT_HASH-build}, %Y-%m-%d"\" >> ${FILE}
 echo BUILD_OPTION=${OPENIPC_RELEASE} >> ${FILE}
 date +TIME_STAMP=%s >> ${FILE}
-
 rm -f ${TARGET_DIR}/usr/bin/gdbserver
 
 CONF="INGENIC_OSDRV_T30=y|LIBV4L=y|MAVLINK_ROUTER=y|WIFIBROADCAST=y"
@@ -16,9 +15,8 @@ if ! grep -q "USES_GLIBC" ${BR2_CONFIG} && ! grep -qP ${CONF} ${BR2_CONFIG}; the
 fi
 
 if grep -q "USES_MUSL" ${BR2_CONFIG}; then
-  NAME=${OPENIPC_RELEASE/lte/fpv}
-  LIST=${BR2_EXTERNAL_SCRIPTS}/excludes/${OPENIPC_MODEL}_${NAME}.list
-  test -e ${LIST} && xargs -a ${LIST} -i rm -rf ${TARGET_DIR}{}
+  LIST=${BR2_EXTERNAL}/scripts/excludes/${OPENIPC_MODEL}_${OPENIPC_RELEASE}.list
+  test -e ${LIST} && xargs -a ${LIST} -I % rm -rf ${TARGET_DIR}/%
 
   ln -sf /lib/libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
   ln -sf ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
