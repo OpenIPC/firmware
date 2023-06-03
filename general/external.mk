b78a539a39d081dcfcfee782c74c0977f09d53cb
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-06-03 04:37:54 +0200

    Makefile: add variable for output directory

--- general/external.mk
+++ general/external.mk
@@ -3,7 +3,7 @@ export OPENIPC_MODEL = $(call qstrip,$(BR2_OPENIPC_MODEL))
 export OPENIPC_FAMILY = $(call qstrip,$(BR2_OPENIPC_FAMILY))
 export OPENIPC_RELEASE = $(call qstrip,$(BR2_OPENIPC_RELEASE))
 
-export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(BR2_OPENIPC_VENDOR)
+export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_VENDOR)
 OPENIPC_TOOLCHAIN = latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
