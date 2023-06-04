68d05e2e129b7b06d5f5630cbfa5f5c0de9c88d6
cronyx <xcronyx@gmail.com>
2023-06-04 21:35:21 +0300

    build imx307 4l from opensdk (#842)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -32,6 +32,7 @@ endef
 define HISILICON_OPENSDK_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
 	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx307/libsns_imx307.so $(TARGET_DIR)/usr/lib/sensors
 endef
 
 $(eval $(kernel-module))
