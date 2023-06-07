fa10f7b8b86d25efa19dbf27505f5b74d8f59e24
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-06-07 09:13:03 +0200

    General: rename openipc vendor variables

--- general/external.mk
+++ general/external.mk
@@ -1,9 +1,9 @@
-export OPENIPC_VENDOR = $(call qstrip,$(BR2_OPENIPC_VENDOR))
-export OPENIPC_MODEL = $(call qstrip,$(BR2_OPENIPC_MODEL))
-export OPENIPC_FAMILY = $(call qstrip,$(BR2_OPENIPC_FAMILY))
-export OPENIPC_RELEASE = $(call qstrip,$(BR2_OPENIPC_RELEASE))
+export OPENIPC_SOC_VENDOR = $(call qstrip,$(BR2_OPENIPC_SOC_VENDOR))
+export OPENIPC_SOC_MODEL = $(call qstrip,$(BR2_OPENIPC_SOC_MODEL))
+export OPENIPC_SOC_FAMILY = $(call qstrip,$(BR2_OPENIPC_SOC_FAMILY))
+export OPENIPC_FLAVOR = $(call qstrip,$(BR2_OPENIPC_FLAVOR))
 
-export BR2_EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_VENDOR)
 OPENIPC_TOOLCHAIN = latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
+EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
 
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
