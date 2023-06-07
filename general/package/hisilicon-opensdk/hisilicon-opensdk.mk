444039d80763ec57d1e4363dc3a5db29e215f011
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-06-07 09:13:03 +0200

    General: rename openipc vendor variables

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -15,24 +15,24 @@ HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
 	DISABLE_PM=1 \
 	DISABLE_TDE=1 \
 	DISABLE_VO=1 \
-	CHIPARCH=$(OPENIPC_FAMILY)
+	CHIPARCH=$(OPENIPC_SOC_FAMILY)
 
-ifeq ($(OPENIPC_FAMILY),hi3516ev200)
+ifeq ($(OPENIPC_SOC_FAMILY),hi3516ev200)
 	HISILICON_OPENSDK_MODULE_MAKE_OPTS += DISABLE_ISP=1
 	HISILICON_OPENSDK_SDK_CODE = 0x3516E200
-else ifeq ($(OPENIPC_FAMILY),gk7205v200)
+else ifeq ($(OPENIPC_SOC_FAMILY),gk7205v200)
 	HISILICON_OPENSDK_SDK_CODE = 0x7205200
 endif
 
 # for userspace libraries
 define HISILICON_OPENSDK_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CHIPARCH=$(OPENIPC_FAMILY) SDK_CODE=$(HISILICON_OPENSDK_SDK_CODE) -C $(@D)/libraries all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CHIPARCH=$(OPENIPC_SOC_FAMILY) SDK_CODE=$(HISILICON_OPENSDK_SDK_CODE) -C $(@D)/libraries all
 endef
 
 define HISILICON_OPENSDK_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
-	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
-	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx307/libsns_imx307.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_SOC_FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_SOC_FAMILY)/sony_imx307/libsns_imx307.so $(TARGET_DIR)/usr/lib/sensors
 endef
 
 $(eval $(kernel-module))
