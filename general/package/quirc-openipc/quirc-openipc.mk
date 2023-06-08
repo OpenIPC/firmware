aae3716dbde0419f1bbfc89f715300fa73ee14c3
Вячеслав Стерх <xcronyx@gmail.com>
2023-06-08 16:30:54 +0300

    change source url for quirc

--- general/package/quirc-openipc/quirc-openipc.mk
+++ general/package/quirc-openipc/quirc-openipc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 QUIRC_OPENIPC_VERSION = 33251ce2005855392666fd8c7436d8c309a127d7
-QUIRC_OPENIPC_SITE = $(call github,cronyx,quirc,$(QUIRC_OPENIPC_VERSION))
+QUIRC_OPENIPC_SITE = $(call github,openipc,quirc,$(QUIRC_OPENIPC_VERSION))
 QUIRC_OPENIPC_DEPENDENCIES += libjpeg
 QUIRC_OPENIPC_LICENSE = ISC
 QUIRC_OPENIPC_LICENSE_FILES = LICENSE.txt
