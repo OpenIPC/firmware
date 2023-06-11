52f4b1ec5c2632191d88a63917a4724796af5ca1
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-06-11 17:32:01 +0200

    Linux: download archive based on git sha (#873)

--- general/external.mk
+++ general/external.mk
@@ -3,6 +3,7 @@ export OPENIPC_SOC_MODEL = $(call qstrip,$(BR2_OPENIPC_SOC_MODEL))
 export OPENIPC_SOC_FAMILY = $(call qstrip,$(BR2_OPENIPC_SOC_FAMILY))
 export OPENIPC_FLAVOR = $(call qstrip,$(BR2_OPENIPC_FLAVOR))
 
+OPENIPC_KERNEL = $(shell git ls-remote https://github.com/openipc/linux.git $(OPENIPC_SOC_VENDOR)-$(OPENIPC_SOC_FAMILY) | cut -f1)
 OPENIPC_TOOLCHAIN = latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 EXTERNAL_VENDOR = $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
 
