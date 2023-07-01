d0e62fdd0da2c8935fc6b5188210f402b79fbb6e
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-07-01 19:11:52 +0200

    Package: add global variable to request git sha

--- general/package/atbm603x/atbm603x.mk
+++ general/package/atbm603x/atbm603x.mk
@@ -6,7 +6,7 @@
 
 ATBM603X_SITE_METHOD = git
 ATBM603X_SITE = https://github.com/openipc/atbm_60xx
-ATBM603X_VERSION = $(shell git ls-remote $(ATBM603X_SITE) HEAD | head -1 | awk '{ print $$1 }')
+ATBM603X_VERSION = $(call EXTERNAL_SHA,$(ATBM603X_SITE),HEAD)
 
 ATBM603X_LICENSE = GPL-2.0
 
