08facb702def4f72bd7d665516ae879f5eb199af
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-07-11 21:58:17 +0200

    Makefile: limit kernel size to 2048kb

--- general/package/exfat-openipc/exfat-openipc.mk
+++ general/package/exfat-openipc/exfat-openipc.mk
@@ -4,14 +4,9 @@
 #
 ################################################################################
 
-ifeq ($(LOCAL_DOWNLOAD),y)
 EXFAT_OPENIPC_SITE_METHOD = git
 EXFAT_OPENIPC_SITE = https://github.com/namjaejeon/linux-exfat-oot
-EXFAT_OPENIPC_VERSION = $(shell git ls-remote $(EXFAT_OPENIPC_SITE) HEAD | head -1 | cut -f1)
-else
-EXFAT_OPENIPC_SITE = https://github.com/namjaejeon/linux-exfat-oot/archive
-EXFAT_OPENIPC_SOURCE = master.tar.gz
-endif
+EXFAT_OPENIPC_VERSION = 331d3f5c425717085a825253f6aeb99af6848099
 
 EXFAT_OPENIPC_LICENSE = GPL-2.0
 EXFAT_OPENIPC_LICENSE_FILES = COPYING
