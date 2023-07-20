c65b973603a7a8346ac31c711ae81f6baecec36f
Igor Zalatov <flyrouter@gmail.com>
2023-07-20 10:29:02 +0300

    Update ONVIF package (#917)

--- general/package/onvif-simple-server/onvif-simple-server.mk
+++ general/package/onvif-simple-server/onvif-simple-server.mk
@@ -6,7 +6,7 @@
 
 ONVIF_SIMPLE_SERVER_SITE_METHOD = git
 ONVIF_SIMPLE_SERVER_SITE = https://github.com/roleoroleo/onvif_simple_server
-ONVIF_SIMPLE_SERVER_VERSION = 7dc083fdd2a3cf3465945616c6d8d15e200a5707
+ONVIF_SIMPLE_SERVER_VERSION = 74d4959684341328a5f7467590e1b139332d3497
 
 ONVIF_SIMPLE_SERVER_DEPENDENCIES = mbedtls-openipc
 ONVIF_SIMPLE_SERVER_LICENSE = MIT
@@ -22,6 +22,19 @@ define ONVIF_SIMPLE_SERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(@D)/wsd_simple_server
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/etc $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/onvif.conf
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/device_service_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/device_service_files $(@D)/device_service_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/generic_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/generic_files $(@D)/generic_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/media_service_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media_service_files $(@D)/media_service_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/ptz_service_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/ptz_service_files $(@D)/ptz_service_files/*
+
+	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/device_service
+	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/events_service
+	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/media_service
+	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/ptz_service
 endef
 
 $(eval $(generic-package))
