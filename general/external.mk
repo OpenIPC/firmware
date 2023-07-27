78283eaf1d0be510cdeadb25957b78324156c121
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-07-27 17:27:19 +0200

    Ingenic: separate t21 variants

--- general/external.mk
+++ general/external.mk
@@ -6,12 +6,18 @@ export OPENIPC_FLAVOR := $(call qstrip,$(BR2_OPENIPC_FLAVOR))
 EXTERNAL_VENDOR := $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
 OPENIPC_TOOLCHAIN := latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 
-ifeq ($(call qstrip,$(BR2_DL_DIR)),$(TOPDIR)/dl)
-OPENIPC_KERNEL := $(OPENIPC_SOC_VENDOR)-$(OPENIPC_SOC_FAMILY)
+ifeq ($(OPENIPC_SOC_FAMILY),t21)
+	BR2_OPENIPC_KERNEL := $(OPENIPC_SOC_VENDOR)-t31
 else
-OPENIPC_KERNEL := $(shell git ls-remote https://github.com/openipc/linux \
-	$(OPENIPC_SOC_VENDOR)-$(OPENIPC_SOC_FAMILY) | head -1 | cut -f1)
-LOCAL_DOWNLOAD = y
+	BR2_OPENIPC_KERNEL := $(OPENIPC_SOC_VENDOR)-$(OPENIPC_SOC_FAMILY)
+endif
+
+ifeq ($(call qstrip,$(BR2_DL_DIR)),$(TOPDIR)/dl)
+	OPENIPC_KERNEL := $(BR2_OPENIPC_KERNEL)
+else
+	OPENIPC_KERNEL := $(shell git ls-remote https://github.com/openipc/linux \
+		$(BR2_OPENIPC_KERNEL) | head -1 | cut -f1)
+	LOCAL_DOWNLOAD = y
 endif
 
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
