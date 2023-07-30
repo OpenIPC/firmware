b32995328a1cf739c4c45c2e0f18841ee2e9c8c1
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-07-30 20:23:58 +0200

    Sigmastar: add sensor configs and adjust folder structure

--- general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
+++ general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
@@ -11,14 +11,14 @@ SIGMASTAR_OSDRV_INFINITY6E_LICENSE_FILES = LICENSE
 
 define SIGMASTAR_OSDRV_INFINITY6E_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.84/sigmastar
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/kmod/*.ko
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/kmod/*.ko
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/kmod/*
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensors/kmod/*
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/iqfile
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/iqfile $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/iqfile/iqfile0.bin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/firmware
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/firmware $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensors/firmware/*
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors/venc_fw
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors/venc_fw $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/venc_fw/chagall.bin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensors/configs/*
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/script/*
