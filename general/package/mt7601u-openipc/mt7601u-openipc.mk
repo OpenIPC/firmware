c15805492f371b83d1025b74bbdc884853b7f5d4
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-08-11 22:12:58 +0200

    [no ci] Overlay: copy mediatek configuration files from package

--- general/package/mt7601u-openipc/mt7601u-openipc.mk
+++ general/package/mt7601u-openipc/mt7601u-openipc.mk
@@ -7,11 +7,15 @@
 MT7601U_OPENIPC_SITE_METHOD = git
 MT7601U_OPENIPC_SITE = https://github.com/openipc/mt7601u
 MT7601U_OPENIPC_VERSION = $(shell git ls-remote $(MT7601U_OPENIPC_SITE) HEAD | head -1 | cut -f1)
-
 MT7601U_OPENIPC_LICENSE = GPL-2.0
 
 MT7601U_OPENIPC_MODULE_MAKE_OPTS = \
 	KSRC=$(LINUX_DIR)
+
+define MT7601U_OPENIPC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/mediatek
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/mediatek $(@D)/MT7601USTA.dat
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
