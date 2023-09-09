2f9317e1264e7eee28392425eb561f5c4968408a
Igor Zalatov <flyrouter@gmail.com>
2023-09-10 00:24:50 +0300

    Update and fix config and dir path for onvif server (#989)

--- general/package/onvif-simple-server/onvif-simple-server.mk
+++ general/package/onvif-simple-server/onvif-simple-server.mk
@@ -6,7 +6,7 @@
 
 ONVIF_SIMPLE_SERVER_SITE_METHOD = git
 ONVIF_SIMPLE_SERVER_SITE = https://github.com/roleoroleo/onvif_simple_server
-ONVIF_SIMPLE_SERVER_VERSION = 74d4959684341328a5f7467590e1b139332d3497
+ONVIF_SIMPLE_SERVER_VERSION = b875f42b81db13032070c35d934ed3b46f52101e
 
 ONVIF_SIMPLE_SERVER_DEPENDENCIES = mbedtls-openipc
 ONVIF_SIMPLE_SERVER_LICENSE = MIT
@@ -30,6 +30,8 @@ define ONVIF_SIMPLE_SERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media_service_files $(@D)/media_service_files/*
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/ptz_service_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/ptz_service_files $(@D)/ptz_service_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/wsd_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/wsd_files $(@D)/wsd_files/*
 
 	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/device_service
 	ln -s /usr/sbin/onvif_simple_server $(TARGET_DIR)/var/www/onvif/events_service
