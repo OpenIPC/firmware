4433f85bee6d35207c43696a10f1653a4cfbc95e
gtxaspec <gtxent@gmail.com>
2023-09-10 14:53:24 -0700

    remove local files; update makefile

--- general/package/logcat-openipc/logcat-openipc.mk
+++ general/package/logcat-openipc/logcat-openipc.mk
@@ -2,13 +2,18 @@
 #
 # logcat-openipc
 #
-# https://github.com/gtxaspec/linux_logcat
-#
 ################################################################################
 
-define LOGCAT_OPENIPC_EXTRACT_CMDS
-        cp -av $(LOGCAT_OPENIPC_PKGDIR)/src/* $(@D)/
-endef
+LOGCAT_OPENIPC_SITE = https://github.com/gtxaspec/linux_logcat/archive
+LOGCAT_OPENIPC_SOURCE = master.tar.gz
+
+LOGCAT_OPENIPC_LICENSE = GPL-2.0
+LOGCAT_OPENIPC_LICENSE_FILES = COPYING
+
+#define LOGCAT_OPENIPC_EXTRACT_CMDS
+#        cp -av $(LOGCAT_OPENIPC_PKGDIR)/src/* $(@D)/
+#endef
+
 
 LOGCAT_OPENIPC_INSTALL_STAGING = YES
 
