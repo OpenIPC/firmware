2fa70a9d8cc34a2d10caab7fe2eaeb589e965aed
cronyx <xcronyx@gmail.com>
2023-09-22 03:07:33 +0300

    added mavlink-router package instead of prebuild binary (#1004)

--- general/package/mavlink-router/mavlink-router.mk
+++ general/package/mavlink-router/mavlink-router.mk
@@ -4,11 +4,30 @@
 #
 ################################################################################
 
+MAVLINK_ROUTER_VERSION = 18c06da77563db9ba1b7c4173ff5cae1646b383e
+MAVLINK_ROUTER_SITE = $(call github,mavlink-router,mavlink-router,$(MAVLINK_ROUTER_VERSION))
 MAVLINK_ROUTER_LICENSE = ASF-2.0
 
+MAVLINK_ROUTER_DEPENDENCIES += host-meson host-pkgconf
+
+MAVLINK_ROUTER_CONF_OPTS += \
+	--buildtype=release \
+	--cross-file $(MAVLINK_ROUTER_PKGDIR)/files/meson-cross-$(ARCH)-openipc.ini
+
+define MAVLINK_ROUTER_CONFIGURE_CMDS
+	rm -rf $(@D)/build
+	mkdir -p $(@D)/build
+	cp -f $(MAVLINK_ROUTER_PKGDIR)/files/prepare.sh $(@D)/prepare.sh
+	cd $(@D) ; sh prepare.sh
+	$(TARGET_MAKE_ENV) meson setup $(MAVLINK_ROUTER_CONF_OPTS) $(@D) $(@D)/build
+endef
+
+define MAVLINK_ROUTER_BUILD_CMDS
+	$(TARGET_MAKE_ENV) ninja -C $(@D)/build
+endef
+
 define MAVLINK_ROUTER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/bin
-	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink-routerd $(TARGET_DIR)/usr/bin/mavlink-routerd
+	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) ninja -C $(@D)/build install
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
 	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink.conf $(TARGET_DIR)/etc/mavlink.conf
--- general/package/mavlink-router/readme.md
+++
@@ -1,7 +0,0 @@
-# MAVLink Router Package
-
-Все необходимые настройки находятся в /etc/mavlink.conf
-
-Запуск и остановка сервиса /etc/init.d/S97mavlink [start|stop]
-
-Полный список всех возможных [настроек](https://github.com/mavlink-router/mavlink-router/blob/master/examples/config.sample) можно получить из сэмпла конфига с официального гитхаба. 
