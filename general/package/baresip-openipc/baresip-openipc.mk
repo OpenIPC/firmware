b6dbe964f7ace4760aa11f9d1316473b3ccd83a6
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-09-29 18:01:57 +0200

    Packages: adjust baresip-openipc (#1011)

--- general/package/baresip-openipc/baresip-openipc.mk
+++ general/package/baresip-openipc/baresip-openipc.mk
@@ -10,19 +10,19 @@ BARESIP_OPENIPC_SITE = https://github.com/baresip/baresip/archive
 BARESIP_OPENIPC_DEPENDENCIES = libre-openipc librem-openipc zlib
 
 BARESIP_OPENIPC_MAKE_ENV = \
-		$(TARGET_CONFIGURE_OPTS) \
-		SYSROOT=$(STAGING_DIR)/usr \
-		SYSROOT_LOCAL=$(STAGING_DIR)/usr \
-		LD="$(TARGET_CC)" \
-		LFLAGS="$(TARGET_LDFLAGS)"
+	$(TARGET_CONFIGURE_OPTS) \
+	SYSROOT=$(STAGING_DIR)/usr \
+	SYSROOT_LOCAL=$(STAGING_DIR)/usr \
+	LD="$(TARGET_CC)" \
+	LFLAGS="$(TARGET_LDFLAGS)"
 
 BARESIP_OPENIPC_MAKE_ARGS = \
-		LIBRE_MK="$(STAGING_DIR)/usr/share/re/re.mk" \
-		LIBRE_INC="$(STAGING_DIR)/usr/include/re" \
-		LIBRE_SO="$(STAGING_DIR)/usr/lib" \
-		LIBREM_MK="$(STAGING_DIR)/usr/share/rem/rem.mk" \
-		LIBREM_INC="$(STAGING_DIR)/usr/include/rem" \
-		LIBREM_SO="$(STAGING_DIR)/usr/lib"
+	LIBRE_MK="$(STAGING_DIR)/usr/share/re/re.mk" \
+	LIBRE_INC="$(STAGING_DIR)/usr/include/re" \
+	LIBRE_SO="$(STAGING_DIR)/usr/lib" \
+	LIBREM_MK="$(STAGING_DIR)/usr/share/rem/rem.mk" \
+	LIBREM_INC="$(STAGING_DIR)/usr/include/rem" \
+	LIBREM_SO="$(STAGING_DIR)/usr/lib"
 
 ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
 BARESIP_OPENIPC_DEPENDENCIES += alsa-lib
@@ -31,7 +31,7 @@ endif
 ifeq ($(BR2_PACKAGE_FFMPEG_SWSCALE),y)
 BARESIP_OPENIPC_DEPENDENCIES += ffmpeg
 BARESIP_OPENIPC_MAKE_ENV += \
-		CFLAGS+=-DCODEC_TYPE_VIDEO=AVMEDIA_TYPE_VIDEO
+	CFLAGS+=-DCODEC_TYPE_VIDEO=AVMEDIA_TYPE_VIDEO
 endif
 
 ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
@@ -51,21 +51,21 @@ BARESIP_OPENIPC_DEPENDENCIES += xlib_libXext
 endif
 
 define BARESIP_OPENIPC_BUILD_CMDS
-		$(BARESIP_OPENIPC_MAKE_ENV) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) all
+	$(BARESIP_OPENIPC_MAKE_ENV) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) all
 endef
 
 define BARESIP_OPENIPC_CLEAN_CMDS
-		$(BARESIP_OPENIPC_MAKE_ENV) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) clean
+	$(BARESIP_OPENIPC_MAKE_ENV) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) clean
 endef
 
 define BARESIP_OPENIPC_INSTALL_TARGET_CMDS
-		$(BARESIP_OPENIPC_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) install
+	$(BARESIP_OPENIPC_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(MAKE) $(BARESIP_OPENIPC_MAKE_ARGS) -C $(@D) install
 endef
 
 define BARESIP_OPENIPC_UNINSTALL_TARGET_CMDS
-		$(RM) $(TARGET_DIR)/usr/bin/baresip
-		$(RM) -r $(TARGET_DIR)/usr/lib/baresip
-		$(RM) -r $(TARGET_DIR)/usr/share/baresip
+	$(RM) $(TARGET_DIR)/usr/bin/baresip
+	$(RM) -r $(TARGET_DIR)/usr/lib/baresip
+	$(RM) -r $(TARGET_DIR)/usr/share/baresip
 endef
 
 $(eval $(generic-package))
--- general/package/libre-openipc/libre-openipc.mk
+++ general/package/libre-openipc/libre-openipc.mk
@@ -1,8 +1,8 @@
-############################################################################
+#############################################################
 #
 # libre-openipc
 #
-###########################################################################
+#############################################################
 
 LIBRE_OPENIPC_VERSION = 2.1.1
 LIBRE_OPENIPC_SOURCE = v$(LIBRE_OPENIPC_VERSION).tar.gz
@@ -13,45 +13,45 @@ LIBRE_OPENIPC_INSTALL_STAGING = YES
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 LIBRE_OPENIPC_DEPENDENCIES += openssl
 endif
+
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 LIBRE_OPENIPC_DEPENDENCIES += zlib
 endif
 
 define LIBRE_OPENIPC_BUILD_CMDS
-$(TARGET_MAKE_ENV) \
-	$(MAKE) -C $(@D) \
-		LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
-		LIBRE_INC=$(STAGING_DIR)/usr/include/re \
-		LIBRE_SO=$(STAGING_DIR)/usr/lib \
-		CC="$(TARGET_CC)" \
-		EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE -fPIC -DOPENBSD" \
-		EXTRA_LFLAGS="-lm" \
-		DESTDIR="$(STAGING_DIR)" \
-		SYSROOT="$(STAGING_DIR)/usr" \
-		SYSROOT_ALT="$(STAGING_DIR)/usr" \
-		RELEASE=1 \
-		CROSS_COMPILE="$(TARGET_CROSS)" \
-		OS=linux \
-		all install
+$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+	LIBRE_MK=$(STAGING_DIR)/usr/share/re/re.mk \
+	LIBRE_INC=$(STAGING_DIR)/usr/include/re \
+	LIBRE_SO=$(STAGING_DIR)/usr/lib \
+	CC="$(TARGET_CC)" \
+	EXTRA_CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE -fPIC -DOPENBSD" \
+	EXTRA_LFLAGS="-lm" \
+	DESTDIR="$(STAGING_DIR)" \
+	SYSROOT="$(STAGING_DIR)/usr" \
+	SYSROOT_ALT="$(STAGING_DIR)/usr" \
+	RELEASE=1 \
+	CROSS_COMPILE="$(TARGET_CROSS)" \
+	OS=linux \
+	all install
 endef
 
 define LIBRE_OPENIPC_INSTALL_STAGING_CMDS
-		$(INSTALL) -D -m 644 $(@D)/include/re.h $(STAGING_DIR)/usr/include/re/re.h
+	$(INSTALL) -D -m 644 $(@D)/include/re.h $(STAGING_DIR)/usr/include/re/re.h
 endef
 
 define LIBRE_OPENIPC_INSTALL_TARGET_CMDS
-		$(INSTALL) -m 644 -D $(@D)/libre.so $(TARGET_DIR)/usr/lib/libre.so
-		ln -sf libre.so $(TARGET_DIR)/usr/lib/libre.so.2
+	$(INSTALL) -m 644 -D $(@D)/libre.so $(TARGET_DIR)/usr/lib/libre.so
+	ln -sf libre.so $(TARGET_DIR)/usr/lib/libre.so.2
 endef
 
 define LIBRE_OPENIPC_UNINSTALL_STAGING_CMDS
-		$(RM) -r $(STAGING_DIR)/usr/include/re
-		$(RM) $(STAGING_DIR)/usr/lib/libre.a
-		$(RM) $(STAGING_DIR)/usr/lib/libre.so
+	$(RM) -r $(STAGING_DIR)/usr/include/re
+	$(RM) $(STAGING_DIR)/usr/lib/libre.a
+	$(RM) $(STAGING_DIR)/usr/lib/libre.so
 endef
 
 define LIBRE_OPENIPC_UNINSTALL_TARGET_CMDS
-		$(RM) $(TARGET_DIR)/usr/lib/libre.so
+	$(RM) $(TARGET_DIR)/usr/lib/libre.so
 endef
 
 $(eval $(generic-package))
