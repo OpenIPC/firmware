c61c02e64730e582099549390165620b986e4d7c
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-10-05 12:30:42 +0100

    Update venc-openipc.mk
    created commands to include the new files for venc

--- general/package/venc-openipc/venc-openipc.mk
+++ general/package/venc-openipc/venc-openipc.mk
@@ -9,6 +9,9 @@ VENC_OPENIPC_LICENSE = MIT
 define VENC_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	cp $(VENC_OPENIPC_PKGDIR)/files/venc $(TARGET_DIR)/usr/bin/venc
+	cp $(VENC_OPENIPC_PKGDIR)/files/S98venc $(TARGET_DIR)/etc/init.d/S98venc
+	cp $(VENC_OPENIPC_PKGDIR)/files/channels.sh $(TARGET_DIR)/root/channels/sh
+	cp $(VENC_OPENIPC_PKGDIR)/files/telemetry $(TARGET_DIR)/usr/bin/telemetry
 endef
 
 $(eval $(generic-package))
