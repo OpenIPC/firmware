7e81d5794b3599360aec12fbec9e7917861ea1dc
cronyx <xcronyx@gmail.com>
2023-10-13 21:28:38 +0300

    remove venc & vdec binaries from packages (#1045)

--- general/package/vdec-openipc/vdec-openipc.mk
+++ general/package/vdec-openipc/vdec-openipc.mk
@@ -4,11 +4,16 @@
 #
 ################################################################################
 
+VDEC_OPENIPC_SITE = https://github.com/OpenIPC/silicon_research/releases/download/latest/vdec
+
 VDEC_OPENIPC_LICENSE = MIT
 
 define VDEC_OPENIPC_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	cp $(VDEC_OPENIPC_PKGDIR)/files/vdec $(TARGET_DIR)/usr/bin/vdec
+	curl -k -L -o $(@D)/vdec $(VDEC_OPENIPC_SITE)
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/vdec
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	cp $(VDEC_OPENIPC_PKGDIR)/files/S98vdec $(TARGET_DIR)/etc/init.d/S98vdec
 endef
 
 $(eval $(generic-package))
