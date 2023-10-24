5ab574e7a4a33b84e7f6addf208211de0c0fc4d5
cronyx <xcronyx@gmail.com>
2023-10-24 17:09:34 +0300

    split air and ground mavlink configs (#1087)

--- general/package/mavlink-router/mavlink-router.mk
+++ general/package/mavlink-router/mavlink-router.mk
@@ -12,6 +12,11 @@ MAVLINK_ROUTER_LICENSE = ASF-2.0
 MAVLINK_ROUTER_DEPENDENCIES += host-meson host-pkgconf
 MAVLINK_ROUTER_GIT_SUBMODULES = YES
 
+MAVLINK_ROUTER_UNIT = drone
+ifeq ($(OPENIPC_SOC_FAMILY),hi3536dv100)
+	MAVLINK_ROUTER_UNIT = gs
+endif
+
 ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
 MAVLINK_ROUTER_LIBC = glibc
 else
@@ -38,7 +43,7 @@ define MAVLINK_ROUTER_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) ninja -C $(@D)/build install
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink.conf $(TARGET_DIR)/etc/mavlink.conf
+	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink_$(MAVLINK_ROUTER_UNIT).conf $(TARGET_DIR)/etc/mavlink.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	cp $(MAVLINK_ROUTER_PKGDIR)/files/S97mavlink $(TARGET_DIR)/etc/init.d
