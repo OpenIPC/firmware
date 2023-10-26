6e8f9236755d190e7451f5bce04f3864a01a7860
cronyx <xcronyx@gmail.com>
2023-10-26 21:46:48 +0300

    261023 cfg vdec (#1090)

--- general/package/vdec-openipc/vdec-openipc.mk
+++ general/package/vdec-openipc/vdec-openipc.mk
@@ -16,6 +16,9 @@ define VDEC_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(VDEC_OPENIPC_PKGDIR)/files/S98vdec
 
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(VDEC_OPENIPC_PKGDIR)/files/vdec.conf
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/vdec/vdec
 endef
