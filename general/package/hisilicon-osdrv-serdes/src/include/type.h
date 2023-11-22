a93a10f61c944449addc6beeed22363d766fbf8d
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-11-22 18:22:40 +0100

    [no ci] Hisilicon: add hisilicon-osdrv-serdes package (#1154)
    Co-authored-by: Signor Pellegrino <68112357+FlyRouter@users.noreply.github.com>

---
+++ general/package/hisilicon-osdrv-serdes/src/include/type.h
@@ -0,0 +1,51 @@
+/*
+ * copyright (c) hisilicon technologies co., ltd. 2016-2019. all rights reserved.
+ * description: type.h
+ * author: hisilicon multimedia software group
+ * create: 2019-05-17
+ */
+
+#ifndef __TYPE_H__
+#define __TYPE_H__
+
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+/*----------------------------------------------*
+ * const defination                             *
+ *----------------------------------------------*/
+
+#ifndef NULL
+    #define NULL    0L
+#endif
+
+#ifndef HI_FALSE
+    #define HI_FALSE   0
+#endif
+
+#ifndef HI_TRUE
+    #define HI_TRUE    1
+#endif
+
+#ifndef HI_SUCCESS
+#define HI_SUCCESS  0
+#endif
+
+#ifndef  HI_FAILURE
+#define HI_FAILURE  (-1)
+#endif
+
+typedef unsigned char HI_U8;
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+#endif /* __TYPE_H__ */
+
