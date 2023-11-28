c35e6eb414de43191ee014e5ee3c1e67b012fda1
gtxaspec <gtxent@gmail.com>
2023-11-27 19:11:45 -0800

    update module opts

--- general/package/ingenic-libimp-control/ingenic-libimp-control.mk
+++ general/package/ingenic-libimp-control/ingenic-libimp-control.mk
@@ -10,13 +10,12 @@ INGENIC_LIBIMP_CONTROL_LICENSE = GPL-2.0
 INGENIC_LIBIMP_CONTROL_LICENSE_FILES = COPYING
 
 define INGENIC_LIBIMP_CONTROL_BUILD_CMDS
-    $(MAKE) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)
+	$(MAKE) CONFIG_SOC=$(OPENIPC_SOC_MODEL) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)
 endef
 
 define INGENIC_LIBIMP_CONTROL_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/libimp_control.so $(TARGET_DIR)/usr/lib
-    $(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(INGENIC_LIBIMP_CONTROL_PKGDIR)/src/imp-control.sh
-
+	$(INSTALL) -D -m 0755 $(@D)/libimp_control.so $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(INGENIC_LIBIMP_CONTROL_PKGDIR)/src/imp-control.sh
 endef
 
 $(eval $(generic-package))
