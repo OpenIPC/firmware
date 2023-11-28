864ead52fd020581875f8402050b630b0839220c
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-11-29 00:18:01 +0100

    [no ci] General: remove duplicate json-c package

--- general/package/uqmi-openipc/uqmi.mk
+++ general/package/uqmi-openipc/uqmi.mk
@@ -6,8 +6,9 @@
 
 UQMI_OPENIPC_VERSION = f254fc59c710d781eca3ec36e0bff2d8970370fa
 UQMI_OPENIPC_SITE = git://git.openwrt.org/project/uqmi.git
-UQMI_OPENIPC_DEPENDENCIES = json-c-openipc libubox
+
 UQMI_OPENIPC_LICENSE = LGPL-2.0+
 UQMI_OPENIPC_LICENSE_FILES = COPYING
+UQMI_OPENIPC_DEPENDENCIES = json-c libubox
 
 $(eval $(cmake-package))
