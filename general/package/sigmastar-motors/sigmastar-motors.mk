e5b6f12b4a19f1d214cd1b6c6f54a46cd9479e10
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-01-06 23:43:50 +0100

    [no ci] Sigmastar: simplify motors makefile

--- general/package/sigmastar-motors/sigmastar-motors.mk
+++ general/package/sigmastar-motors/sigmastar-motors.mk
@@ -4,16 +4,7 @@
 #
 ################################################################################
 
-define SIGMASTAR_MOTORS_EXTRACT_CMDS
-	cp -r $(SIGMASTAR_MOTORS_PKGDIR)/src/* $(@D)
-endef
-
-define SIGMASTAR_MOTORS_BUILD_CMDS
-	$(MAKE) CC=$(TARGET_CC) -C $(@D)
-endef
-
-define SIGMASTAR_MOTORS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/output/*
-endef
+SIGMASTAR_MOTORS_SITE_METHOD = local
+SIGMASTAR_MOTORS_SITE = $(SIGMASTAR_MOTORS_PKGDIR)/src
 
 $(eval $(generic-package))
