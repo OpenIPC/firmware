02d4f95a83ae7138e5b41108e40cf1706b9da2bf
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-01-13 23:56:52 +0100

    Package: update majestic-plugins.mk (#1253)

--- general/package/majestic-plugins/majestic-plugins.mk
+++ general/package/majestic-plugins/majestic-plugins.mk
@@ -4,8 +4,23 @@
 #
 ################################################################################
 
+MAJESTIC_PLUGINS_SITE = https://github.com/openipc/majestic-plugins/archive
+MAJESTIC_PLUGINS_SOURCE = master.tar.gz
+
+MAJESTIC_PLUGINS_PATH = $(BASE_DIR)/../majestic-plugins
+
+ifneq ($(wildcard $(MAJESTIC_PLUGINS_PATH)),)
 define MAJESTIC_PLUGINS_BUILD_CMDS
-	$(MAKE) CC=$(TARGET_CC) TARGET=$(OPENIPC_SOC_VENDOR) -C $(BASE_DIR)/../majestic-plugins -B
+	$(MAKE) CC=$(TARGET_CC) TARGET=$(OPENIPC_SOC_VENDOR) -C $(MAJESTIC_PLUGINS_PATH) -B
 endef
+else
+define MAJESTIC_PLUGINS_BUILD_CMDS
+	$(MAKE) CC=$(TARGET_CC) TARGET=$(OPENIPC_SOC_VENDOR) -C $(@D) -B
+endef
+
+define MAJESTIC_PLUGINS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib $(@D)/$(OPENIPC_SOC_VENDOR).so
+endef
+endif
 
 $(eval $(generic-package))
