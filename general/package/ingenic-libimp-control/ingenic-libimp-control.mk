021ef0666fcefd0e3da2a532d27eab3a98653afb
gtxaspec <gtxent@gmail.com>
2024-01-14 06:22:55 -0800

    load imp-config preset settings using init script (#1254)

--- general/package/ingenic-libimp-control/ingenic-libimp-control.mk
+++ general/package/ingenic-libimp-control/ingenic-libimp-control.mk
@@ -20,6 +20,8 @@ endef
 define INGENIC_LIBIMP_CONTROL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libimp_control.so $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(INGENIC_LIBIMP_CONTROL_PKGDIR)/src/imp-control.sh
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(INGENIC_LIBIMP_CONTROL_PKGDIR)/src/S96impconfig
 endef
 
 $(eval $(generic-package))
