d70825d942a274509347039da89e975311a2b51f
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-01-21 08:06:17 +0100

    [no ci] Ingenic: cleanup t20 osdrv modules

--- general/package/ingenic-osdrv-t20/ingenic-osdrv-t20.mk
+++ general/package/ingenic-osdrv-t20/ingenic-osdrv-t20.mk
@@ -12,7 +12,6 @@ INGENIC_OSDRV_T20_LICENSE_FILES = LICENSE
 define INGENIC_OSDRV_T20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/*.yaml
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/*.bin
 
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/jxf22.bin
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/jxf23.bin
@@ -22,15 +21,8 @@ define INGENIC_OSDRV_T20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/sc2232.bin
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/sc2235.bin
 
-        # Modules for T20 are built from the openingenic repository sources, so we don't need to copy them.
-	#$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.14/ingenic
-	#ln -sf tx-isp-t20.ko $(TARGET_DIR)/lib/modules/3.10.14/ingenic/tx-isp-t20l.ko
-
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T20_PKGDIR)/files/script/load*
-
-	# $(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors/params
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/sensors/params $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/*.bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(INGENIC_OSDRV_T20_PKGDIR)/files/lib/*.so
