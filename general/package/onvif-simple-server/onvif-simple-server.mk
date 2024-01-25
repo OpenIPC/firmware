722a64d2f54ecffd96b651d97079665d157276b5
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-01-25 09:05:41 +0100

    Package: set onvif simple server to a fixed commit

--- general/package/onvif-simple-server/onvif-simple-server.mk
+++ general/package/onvif-simple-server/onvif-simple-server.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ONVIF_SIMPLE_SERVER_SITE = $(call github,roleoroleo,onvif_simple_server,$(ONVIF_SIMPLE_SERVER_VERSION))
-ONVIF_SIMPLE_SERVER_VERSION = master
+ONVIF_SIMPLE_SERVER_VERSION = 6a1af547394f1dd528d88b9116b6351bbc736fca
 
 ONVIF_SIMPLE_SERVER_DEPENDENCIES = mbedtls
 ONVIF_SIMPLE_SERVER_LICENSE = MIT
