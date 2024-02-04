0b35b0c85d430359da942ea965f7e322a868bd76
gtxaspec <gtxent@gmail.com>
2024-02-04 07:06:59 -0800

    remove libimp-control; deprecated; moving to plugins (#1297)

--- general/package/ingenic-libimp-control/src/imp-control.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script provides a control interface for the libimp_control library.
-# It sends commands to the server listening on localhost port 4000.
-
-# Send the command and parameters to the server
-echo "imp_control $*" | nc localhost 4000
