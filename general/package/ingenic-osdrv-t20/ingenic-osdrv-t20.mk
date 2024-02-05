a786937cb909e08b1d2e268c573b33aef057fe8b
cronyx <xcronyx@gmail.com>
2024-02-05 23:07:56 +0300

    [ingenic] sensor selection; t30 switch to opensdk

--- general/package/ingenic-osdrv-t20/ingenic-osdrv-t20.mk
+++ general/package/ingenic-osdrv-t20/ingenic-osdrv-t20.mk
@@ -9,6 +9,7 @@ INGENIC_OSDRV_T20_SITE =
 INGENIC_OSDRV_T20_LICENSE = MIT
 INGENIC_OSDRV_T20_LICENSE_FILES = LICENSE
 
+ifeq ($(OPENIPC_SNS_MODEL),)
 define INGENIC_OSDRV_T20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/*.yaml
@@ -27,5 +28,18 @@ define INGENIC_OSDRV_T20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(INGENIC_OSDRV_T20_PKGDIR)/files/lib/*.so
 endef
+else
+define INGENIC_OSDRV_T20_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/$(OPENIPC_SNS_MODEL).yaml
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T20_PKGDIR)/files/sensor/params/*$(OPENIPC_SNS_MODEL).bin
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T20_PKGDIR)/files/script/load*
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(INGENIC_OSDRV_T20_PKGDIR)/files/lib/*.so
+endef
+endif
 
 $(eval $(generic-package))
