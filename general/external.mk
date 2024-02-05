8dba0895a9262f4f4321602e5ad07357c402cc4f
cronyx <xcronyx@gmail.com>
2024-02-06 00:14:55 +0300

    [ingenic] sensor selection; t30 switch to opensdk (#1301)

--- general/external.mk
+++ general/external.mk
@@ -1,6 +1,7 @@
 export OPENIPC_SOC_VENDOR := $(call qstrip,$(BR2_OPENIPC_SOC_VENDOR))
 export OPENIPC_SOC_MODEL := $(call qstrip,$(BR2_OPENIPC_SOC_MODEL))
 export OPENIPC_SOC_FAMILY := $(call qstrip,$(BR2_OPENIPC_SOC_FAMILY))
+export OPENIPC_SNS_MODEL := $(call qstrip,$(BR2_OPENIPC_SNS_MODEL))
 export OPENIPC_FLAVOR := $(call qstrip,$(BR2_OPENIPC_FLAVOR))
 
 EXTERNAL_VENDOR := $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
