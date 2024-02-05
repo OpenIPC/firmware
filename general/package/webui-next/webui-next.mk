0f0c71989bf84f39a44922a3eb6461cc2d67d9a1
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-02-05 01:06:22 +0100

    [no ci] Package: update webui-next install path

--- general/package/webui-next/webui-next.mk
+++ general/package/webui-next/webui-next.mk
@@ -11,10 +11,10 @@ WEBUI_NEXT_LICENSE_FILES = LICENSE
 
 define WEBUI_NEXT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr
-	cp -rv $(@D)/files/usr/sbin $(TARGET_DIR)/usr
+	cp -r $(@D)/sbin $(TARGET_DIR)/usr
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/var
-	cp -rv $(@D)/files/var/www $(TARGET_DIR)/var
+	cp -r $(@D)/www $(TARGET_DIR)/var
 endef
 
 $(eval $(generic-package))
