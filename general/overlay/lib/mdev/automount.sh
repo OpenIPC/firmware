3502b4733de2783bd1fe5141cb1ebc655c81c703
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-02-07 16:18:43 +0100

    Overlay: minor script refactoring (#1304)

--- general/overlay/lib/mdev/automount.sh
+++ general/overlay/lib/mdev/automount.sh
@@ -2,17 +2,15 @@
 
 destdir=/mnt
 
-my_umount()
-{
-	if grep -qs "^/dev/$1 " /proc/mounts ; then
-		umount "${destdir}/$1";
+my_umount() {
+	if grep -qs "^/dev/$1 " /proc/mounts; then
+		umount "${destdir}/$1"
 	fi
 
 	[ -d "${destdir}/$1" ] && rmdir "${destdir}/$1"
 }
 
-my_mount()
-{
+my_mount() {
 	mkdir -p "${destdir}/$1" || exit 1
 
 	if ! mount -t auto -o sync "/dev/$1" "${destdir}/$1"; then
@@ -25,18 +23,19 @@ my_mount()
 	[ -d "${destdir}/$1/autoconfig" ] && cp -afv ${destdir}/$1/autoconfig/* / | logger -s -p daemon.info -t autoconfig
 
 	# execution of the specified commands one time
-	[ -f "${destdir}/$1/autoconfig.sh" ] && (sh ${destdir}/$1/autoconfig.sh ; rm -f ${destdir}/$1/autoconfig.sh) | logger -s -p daemon.info -t autoconfig
+	[ -f "${destdir}/$1/autoconfig.sh" ] && (sh ${destdir}/$1/autoconfig.sh; rm -f ${destdir}/$1/autoconfig.sh) | logger -s -p daemon.info -t autoconfig
 
 	# execution of the specified commands
 	[ -f "${destdir}/$1/autostart.sh" ] && sh ${destdir}/$1/autostart.sh | logger -s -p daemon.info -t autostart
 }
 
 case "${ACTION}" in
-add|"")
-	my_umount ${MDEV}
-	my_mount ${MDEV}
-	;;
-remove)
-	my_umount ${MDEV}
-	;;
+	add|"")
+		my_umount ${MDEV}
+		my_mount ${MDEV}
+		;;
+
+	remove)
+		my_umount ${MDEV}
+		;;
 esac
