8bf265e08ddab883e4ba196a853efe4de43007ec
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-02-08 03:36:44 +0100

    Package: minor refactoring (#1305)

--- general/package/majestic/majestic.mk
+++ general/package/majestic/majestic.mk
@@ -5,20 +5,20 @@
 ################################################################################
 
 MAJESTIC_SITE = https://openipc.s3-eu-west-1.amazonaws.com
-MAJESTIC_SOURCE = majestic.$(MAJESTIC_FAMILY).$(MAJESTIC_RELEASE).master.tar.bz2
+MAJESTIC_SOURCE = majestic.$(MAJESTIC_FAMILY).$(MAJESTIC_VARIANT).master.tar.bz2
 MAJESTIC_LICENSE = PROPRIETARY
 MAJESTIC_LICENSE_FILES = LICENSE
 
 MAJESTIC_FAMILY = $(OPENIPC_SOC_FAMILY)
-MAJESTIC_RELEASE = $(OPENIPC_FLAVOR)
+MAJESTIC_VARIANT = $(OPENIPC_VARIANT)
 
 MAJESTIC_ULTIMATE = hi3516av100 hi3519v101 t20 t21 t40
 ifneq ($(filter $(MAJESTIC_ULTIMATE),$(MAJESTIC_FAMILY)),)
-	MAJESTIC_RELEASE = lite
+	MAJESTIC_VARIANT = lite
 endif
 
-ifeq ($(MAJESTIC_RELEASE),lte)
-	MAJESTIC_RELEASE = fpv
+ifeq ($(MAJESTIC_VARIANT),lte)
+	MAJESTIC_VARIANT = fpv
 endif
 
 MAJESTIC_VENDOR = hisilicon goke ingenic sigmastar
--- general/scripts/rootfs_script.sh
+++ general/scripts/rootfs_script.sh
@@ -4,7 +4,7 @@ FILE=${TARGET_DIR}/usr/lib/os-release
 
 echo OPENIPC_VERSION=${DATE:0:1}.${DATE:1} >> ${FILE}
 date +GITHUB_VERSION="\"${GIT_BRANCH-local}+${GIT_HASH-build}, %Y-%m-%d"\" >> ${FILE}
-echo BUILD_OPTION=${OPENIPC_FLAVOR} >> ${FILE}
+echo BUILD_OPTION=${OPENIPC_VARIANT} >> ${FILE}
 date +TIME_STAMP=%s >> ${FILE}
 
 CONF="INGENIC_OSDRV_T30=y|LIBV4L=y|MAVLINK_ROUTER=y|WIFIBROADCAST=y|WEBRTC_AUDIO_PROCESSING=y"
@@ -13,7 +13,7 @@ if ! grep -q "USES_GLIBC" ${BR2_CONFIG} && ! grep -qP ${CONF} ${BR2_CONFIG}; the
 fi
 
 if grep -q "USES_MUSL" ${BR2_CONFIG}; then
-	LIST=${BR2_EXTERNAL}/scripts/excludes/${OPENIPC_SOC_MODEL}_${OPENIPC_FLAVOR}.list
+	LIST=${BR2_EXTERNAL}/scripts/excludes/${OPENIPC_SOC_MODEL}_${OPENIPC_VARIANT}.list
 	test -e ${LIST} && xargs -a ${LIST} -I % rm -f ${TARGET_DIR}%
 
 	ln -sf libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
