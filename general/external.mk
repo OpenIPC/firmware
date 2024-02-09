fa59efce4473436e9ffded53ec96bb1150295614
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-02-10 00:19:19 +0100

    [no ci] Configs: set separate variable for Majestic version

--- general/external.mk
+++ general/external.mk
@@ -3,6 +3,7 @@ export OPENIPC_SOC_MODEL := $(call qstrip,$(BR2_OPENIPC_SOC_MODEL))
 export OPENIPC_SOC_FAMILY := $(call qstrip,$(BR2_OPENIPC_SOC_FAMILY))
 export OPENIPC_SNS_MODEL := $(call qstrip,$(BR2_OPENIPC_SNS_MODEL))
 export OPENIPC_VARIANT := $(call qstrip,$(BR2_OPENIPC_VARIANT))
+export OPENIPC_MAJESTIC := $(call qstrip,$(BR2_OPENIPC_MAJESTIC))
 
 EXTERNAL_VENDOR := $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
 OPENIPC_TOOLCHAIN := latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
