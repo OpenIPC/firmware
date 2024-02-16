32cc2322700f975aaa4c4272ee35114bc1f0fd6d
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-02-16 14:55:51 +0100

    Package: rename webui-next (#1317)

--- general/package/majestic-webui/majestic-webui.mk
+++ general/package/majestic-webui/majestic-webui.mk
@@ -1,15 +1,15 @@
 ################################################################################
 #
-# webui-next
+# majestic-webui
 #
 ################################################################################
 
-WEBUI_NEXT_SITE = $(call github,openipc,webui-next,$(WEBUI_NEXT_VERSION))
-WEBUI_NEXT_VERSION = master
-WEBUI_NEXT_LICENSE = MIT
-WEBUI_NEXT_LICENSE_FILES = LICENSE
+MAJESTIC_WEBUI_SITE = $(call github,openipc,majestic-webui,$(MAJESTIC_WEBUI_VERSION))
+MAJESTIC_WEBUI_VERSION = master
+MAJESTIC_WEBUI_LICENSE = MIT
+MAJESTIC_WEBUI_LICENSE_FILES = LICENSE
 
-define WEBUI_NEXT_INSTALL_TARGET_CMDS
+define MAJESTIC_WEBUI_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr
 	cp -r $(@D)/sbin $(TARGET_DIR)/usr
 
