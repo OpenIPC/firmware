fee4fc2d0ac9fcb3a729139f36f41b7f0d814de5
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-03-10 19:34:38 +0100

    [no ci] rubyfpv: adjust makefiles (#1341)

--- general/package/rubyfpv/files/ruby_stop.sh
+++ general/package/rubyfpv/files/ruby_stop.sh
@@ -1,5 +1,5 @@
-pidof ruby_start | xargs kill -9 2>/dev/null
-pidof ruby_rx_commands | xargs kill -9 2>/dev/null
-pidof ruby_tx_telemetry | xargs kill -9 2>/dev/null
-pidof ruby_rt_vehicle | xargs kill -9 2>/dev/null
-pidof ruby_logger | xargs kill -9 2>/dev/null
+killall -q ruby_start
+killall -q ruby_rx_commands
+killall -q ruby_tx_telemetry
+killall -q ruby_rt_vehicle
+killall -q ruby_logger
--- general/package/rubyfpv/rubyfpv.mk
+++ general/package/rubyfpv/rubyfpv.mk
@@ -1,37 +1,30 @@
+################################################################################
+#
+# rubyfpv
+#
+################################################################################
 
-define RUBYFPV_EXTRACT_CMDS
-	cp -avr $(RUBYFPV_PKGDIR)/src/* $(@D)/
-	cp -avr $(RUBYFPV_PKGDIR)/files/* $(@D)/
-endef
+RUBYFPV_SITE_METHOD = local
+RUBYFPV_SITE = $(RUBYFPV_PKGDIR)/src
 
 define RUBYFPV_BUILD_CMDS
-	(cd $(@D); $(TARGET_CC) -s ruby_start.c -o ruby_start)
-	(cd $(@D); $(TARGET_CC) -s ruby_start.c -o ruby_logger)
-	(cd $(@D); $(TARGET_CC) -s ruby_start.c -o ruby_rt_vehicle)
-	(cd $(@D); $(TARGET_CC) -s ruby_start.c -o ruby_tx_telemetry)
-	(cd $(@D); $(TARGET_CC) -s ruby_start.c -o ruby_rx_commands)
+	$(MAKE) CC=$(TARGET_CC) -C $(@D) all
 endef
 
 define RUBYFPV_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(RUBYFPV_PKGDIR)/files/S73ruby
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/ruby
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ruby $(RUBYFPV_PKGDIR)/files/licences/*
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/root/ruby
- 
-	cp $(RUBYFPV_PKGDIR)/files/S73ruby $(TARGET_DIR)/etc/init.d
-	chmod 755 $(TARGET_DIR)/etc/init.d/S73ruby
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(RUBYFPV_PKGDIR)/files/tweaksys
 
-	install -m 0755 -D $(@D)/ruby_start $(TARGET_DIR)/usr/sbin/ruby_start
-	install -m 0755 -D $(@D)/ruby_logger $(TARGET_DIR)/usr/sbin/ruby_logger
-	install -m 0755 -D $(@D)/ruby_rt_vehicle $(TARGET_DIR)/usr/sbin/ruby_rt_vehicle
-	install -m 0755 -D $(@D)/ruby_tx_telemetry $(TARGET_DIR)/usr/sbin/ruby_tx_telemetry
-	install -m 0755 -D $(@D)/ruby_rx_commands $(TARGET_DIR)/usr/sbin/ruby_rx_commands
-
-	cp $(RUBYFPV_PKGDIR)/files/ruby_stop.sh $(TARGET_DIR)/usr/sbin
-	chmod 755 $(TARGET_DIR)/usr/sbin/ruby_stop.sh
-	cp $(RUBYFPV_PKGDIR)/files/tweaksys $(TARGET_DIR)/usr/bin
-	cp -r $(RUBYFPV_PKGDIR)/files/licences $(TARGET_DIR)/root/ruby/
-	cp $(RUBYFPV_PKGDIR)/files/version_ruby_base.txt $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/output/*
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(RUBYFPV_PKGDIR)/files/ruby_stop.sh
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/sbin $(RUBYFPV_PKGDIR)/files/version_ruby_base.txt
 endef
 
 $(eval $(generic-package))
