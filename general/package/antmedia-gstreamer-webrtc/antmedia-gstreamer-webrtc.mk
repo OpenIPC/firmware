6d1b8668ae663f6337a0ee8f366e0bdb839b688e
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2024-04-05 11:19:31 +0100

    Add Ant Media Server WebRTC example
    This component currently uses an Omlet fork of the original
    example code as we needed to add a cmake build.
    
    Trying to progress upstreaming this here at which time we'd
    use the upstream repo
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

---
+++ general/package/antmedia-gstreamer-webrtc/antmedia-gstreamer-webrtc.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# antmedia-gstreaamer-webrtc
+#
+################################################################################
+
+ANTMEDIA_GSTREAMER_WEBRTC_SITE = git@github.com:Omlet-Ltd/AntMedia-Gstreamer-Webrtc.git
+ANTMEDIA_GSTREAMER_WEBRTC_SITE_METHOD = git
+ANTMEDIA_GSTREAMER_WEBRTC_VERSION = aeef10e66587fc8fb13e14c8198f8e3ea8c8346c
+ANTMEDIA_GSTREAMER_WEBRTC_GIT_SUBMODULES = YES
+
+ANTMEDIA_GSTREAMER_WEBRTC_INSTALL_STAGING = YES
+ANTMEDIA_GSTREAMER_WEBRTC_LICENSE = MIT
+ANTMEDIA_GSTREAMER_WEBRTC_LICENSE_FILES = License
+
+ANTMEDIA_GSTREAMER_WEBRTC_MAKE_OPTS += VERBOSE=1
+
+ANTMEDIA_GSTREAMER_WEBRTC_DEPENDENCIES = \
+	libglib2 \
+        gst1-plugins-good \
+        gst1-plugins-bad \
+        json-glib
+
+$(eval $(cmake-package))
