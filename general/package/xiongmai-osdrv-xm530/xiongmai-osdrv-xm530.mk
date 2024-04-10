68b136eba5b5b6b5aefa72ab68fd2e5920854666
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-04-10 07:56:09 +0200

    Update xiongmai-osdrv-xm530.mk

--- general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
+++ general/package/xiongmai-osdrv-xm530/xiongmai-osdrv-xm530.mk
@@ -16,14 +16,14 @@ define XIONGMAI_OSDRV_XM530_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/firmware $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/firmware/hevcenc.fwb
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/*.ko
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/usb/*.ko
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/xm711/*.ko
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.103/xiongmai
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/*.ko
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/usb/*.ko
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103/xiongmai $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/xm711/*.ko
 
-# Dirty hack
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.103+/kernel/net/wireless
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103+/kernel/net/wireless $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/rewrite/cfg80211.ko
+	# Dirty hack
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.103/kernel/net/wireless
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.103/kernel/net/wireless $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/kmod/rewrite/cfg80211.ko
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(XIONGMAI_OSDRV_XM530_PKGDIR)/files/script/load*
