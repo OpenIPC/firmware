5fcdec6006e976c7d18eeee310a9407882802c40
Viktor <35473052+viktorxda@users.noreply.github.com>
2024-04-10 16:23:42 +0200

    [no ci] Package: update mini-snmpd-openipc

--- general/package/mini-snmpd-openipc/mini-snmpd.mk
+++ general/package/mini-snmpd-openipc/mini-snmpd.mk
@@ -4,18 +4,19 @@
 #
 ################################################################################
 
-MINI_SNMPD_OPENIPC_VERSION = 1.4
-MINI_SNMPD_OPENIPC_SITE = $(call github,troglobit,mini-snmpd,v$(MINI_SNMPD_OPENIPC_VERSION))
+MINI_SNMPD_OPENIPC_VERSION = v1.4
+MINI_SNMPD_OPENIPC_SITE = $(call github,troglobit,mini-snmpd,$(MINI_SNMPD_OPENIPC_VERSION))
 
 MINI_SNMPD_OPENIPC_LICENSE = GPL-2.0
 MINI_SNMPD_OPENIPC_LICENSE_FILES = COPYING
 MINI_SNMPD_OPENIPC_AUTORECONF = YES
 
 define MINI_SNMPD_OPENIPC_INSTALL_TARGET_CMDS
-
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	cp $(MINI_SNMPD_OPENIPC_PKGDIR)/files/S50snmpd $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(MINI_SNMPD_OPENIPC_PKGDIR)/files/S50snmpd
 
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/mini_snmpd
 endef
 
 $(eval $(autotools-package))
