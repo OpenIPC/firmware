c542e4c0b1527e37d725f0c22e6e7d3e6a418ee7
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-04-28 19:23:14 +0200

    [no ci] Wifibroadcast: add support for RTL8733BU (#1423)

--- general/package/rtl8733bu-openipc/rtl8733bu-openipc.mk
+++ general/package/rtl8733bu-openipc/rtl8733bu-openipc.mk
@@ -5,7 +5,11 @@
 ################################################################################
 
 RTL8733BU_OPENIPC_SITE = $(call github,openipc,realtek-wlan,$(RTL8733BU_OPENIPC_VERSION))
-RTL8733BU_OPENIPC_VERSION = rtl8733bu
+ifeq ($(OPENIPC_VARIANT),fpv)
+	RTL8733BU_OPENIPC_VERSION = rtl8733bu_fpv
+else
+	RTL8733BU_OPENIPC_VERSION = rtl8733bu
+endif
 
 RTL8733BU_OPENIPC_LICENSE = GPL-2.0
 RTL8733BU_OPENIPC_LICENSE_FILES = COPYING
