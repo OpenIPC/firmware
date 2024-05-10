affb77183a62be778d11061aba938f9004c1b5e7
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-05-10 19:47:46 +0200

    Sigmastar: adjust sensor driver path

--- general/package/sigmastar-osdrv-sensors/sigmastar-osdrv-sensors.mk
+++ general/package/sigmastar-osdrv-sensors/sigmastar-osdrv-sensors.mk
@@ -8,8 +8,8 @@ SIGMASTAR_OSDRV_SENSORS_SITE = $(call github,openipc,sensors,$(SIGMASTAR_OSDRV_S
 SIGMASTAR_OSDRV_SENSORS_VERSION = HEAD
 
 define SIGMASTAR_OSDRV_SENSORS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.84/sigmastar
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.84/sigmastar $(@D)/sigmastar/$(OPENIPC_SOC_FAMILY)/*
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/sigmastar
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/sigmastar $(@D)/sigmastar/$(OPENIPC_SOC_FAMILY)/*
 endef
 
 $(eval $(generic-package))
