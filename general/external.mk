5990bb14c3eb5ff0133df8878d5c5e988244030f
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-10-25 23:36:17 +0200

    [no ci] General: sync buildroot configurations (#1583)

--- general/external.mk
+++ general/external.mk
@@ -4,11 +4,10 @@ export OPENIPC_SOC_FAMILY := $(call qstrip,$(BR2_OPENIPC_SOC_FAMILY))
 export OPENIPC_SNS_MODEL := $(call qstrip,$(BR2_OPENIPC_SNS_MODEL))
 export OPENIPC_VARIANT := $(call qstrip,$(BR2_OPENIPC_VARIANT))
 export OPENIPC_MAJESTIC := $(call qstrip,$(BR2_OPENIPC_MAJESTIC))
+export WGET := wget --show-progress --passive-ftp -nd -t5 -T10
 
 EXTERNAL_VENDOR := $(BR2_EXTERNAL)/../br-ext-chip-$(OPENIPC_SOC_VENDOR)
 OPENIPC_TOOLCHAIN := latest/$(shell $(BR2_EXTERNAL)/scripts/show_toolchains.sh $(BR2_DEFCONFIG))
 OPENIPC_KERNEL := $(OPENIPC_SOC_VENDOR)-$(OPENIPC_SOC_FAMILY)
-
-export WGET := wget --show-progress --passive-ftp -nd -t5 -T10
 
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*.mk))
