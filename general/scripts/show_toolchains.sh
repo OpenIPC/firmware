c755d305b152ace1c6c79d501e897329dbfd5b38
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-10-27 18:31:59 +0100

    [no ci] Scripts: update show_toolchains.sh (#1593)

--- general/scripts/show_toolchains.sh
+++ general/scripts/show_toolchains.sh
@@ -10,10 +10,10 @@ THUMB=$(grep -q "THUMB2=y" "$1" && echo _thumb2)
 FLOAT=$(grep -qE "VFPV3=y|VFPV4=y" "$1" && ! grep -q "ARM_EABI=y" "$1" && echo _hf)
 LINUX=$(grep -oP "HEADERS.+\K\d+.\d+" "$1")
 
-if grep -q "GLIBC=y" "$1"; then
-	LIB=glibc
-elif grep -q "MUSL=y" "$1"; then
+if grep -q "MUSL=y" "$1"; then
 	LIB=musl
+elif grep -q "UCLIBC=y" "$1"; then
+	LIB=uclibc
 fi
 
-echo "${SOC:-arm926t}${THUMB}${FLOAT}-gcc13-${LIB:-uclibc}-${LINUX}"
+echo "${SOC:-arm926t}${THUMB}${FLOAT}-gcc13-${LIB:-glibc}-${LINUX}"
