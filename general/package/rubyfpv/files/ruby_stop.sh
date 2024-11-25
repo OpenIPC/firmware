5428d5ccfa3a7e4abea6e32550467150bd9121eb
Petru Soroaga <petrusoroaga@yahoo.com>
2024-11-25 20:25:29 +0200

    Updated to Ruby version 10.0

--- general/package/rubyfpv/files/ruby_stop.sh
+++ general/package/rubyfpv/files/ruby_stop.sh
@@ -3,3 +3,4 @@ pidof ruby_rx_commands | xargs kill -9 2>/dev/null
 pidof ruby_tx_telemetry | xargs kill -9 2>/dev/null
 pidof ruby_rt_vehicle | xargs kill -9 2>/dev/null
 pidof ruby_logger | xargs kill -9 2>/dev/null
+pidof majestic | xargs kill -9 2>/dev/null
