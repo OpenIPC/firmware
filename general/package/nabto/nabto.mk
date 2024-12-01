9fc16f509ef80d476711a83c1105665114f591bf
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-12-01 09:01:19 +0100

    [no ci] Package: update nabto sdk

--- general/package/nabto/nabto.mk
+++ general/package/nabto/nabto.mk
@@ -1,23 +1,25 @@
 ################################################################################
 #
-# nabto embedded sdk
+# nabto
 #
 ################################################################################
 
-NABTO_VERSION = 5.12.0
+NABTO_VERSION = 5.13.0
 NABTO_SOURCE = nabto-embedded-sdk.zip
 NABTO_SITE = https://github.com/nabto/nabto-embedded-sdk/releases/download/v$(NABTO_VERSION)
 
+NABTO_DEPENDENCIES += \
+	libevent-openipc \
+	mbedtls-openipc
+
 define NABTO_EXTRACT_CMDS
 	$(UNZIP) -d $(@D) $(NABTO_DL_DIR)/$(NABTO_SOURCE)
-	mv $(@D)/nabto-embedded-sdk/* $(@D)/
-	rm -rf $(@D)/nabto-embedded-sdk
+	mv $(@D)/nabto-embedded-sdk/* $(@D)
 endef
 
 NABTO_CONF_OPTS += \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DBUILD_SHARED_LIBS=OFF \
-	-DDEVICE_BUILD_TESTS=OFF \
 	-DDEVICE_BUILD_EXAMPLES=OFF
 
 define NABTO_REMOVE_EXTRA
