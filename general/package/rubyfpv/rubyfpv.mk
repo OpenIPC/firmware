a0187d48ab533e2fe29a8800437e1b0910a7dffe
viktorxda <35473052+viktorxda@users.noreply.github.com>
2024-12-13 12:40:22 +0100

    [no ci] rubyfpv: formatting and restore local tweaksys

--- general/package/rubyfpv/rubyfpv.mk
+++ general/package/rubyfpv/rubyfpv.mk
@@ -1,3 +1,9 @@
+################################################################################
+#
+# rubyfpv
+#
+################################################################################
+
 RUBYFPV_SITE_METHOD = git
 RUBYFPV_SITE = https://github.com/PetruSoroaga/RubyFPV
 RUBYFPV_VERSION = 5ec137401ec338c9273e48a7cb9402d96010b9dd
@@ -5,8 +11,8 @@ RUBYFPV_VERSION = 5ec137401ec338c9273e48a7cb9402d96010b9dd
 RUBYFPV_DEPENDENCIES += libpcap iw
 
 RUBYFPV_MAKE_OPTS = \
-    CC="$(TARGET_CC)" \
-    CXX="$(TARGET_CXX)"
+	CC="$(TARGET_CC)" \
+	CXX="$(TARGET_CXX)"
 
 define RUBYFPV_BUILD_CMDS
 	$(MAKE) $(RUBYFPV_MAKE_OPTS) -C $(@D) vehicle RUBY_BUILD_ENV=openipc
@@ -16,6 +22,15 @@ define RUBYFPV_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(RUBYFPV_PKGDIR)/files/S73ruby
 
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/root/ruby/licenses
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/root/ruby/licenses $(@D)/licenses/*
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware/ath9k_htc
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/firmware/ath9k_htc $(RUBYFPV_PKGDIR)/files/htc_9271.fw.3
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(RUBYFPV_PKGDIR)/files/tweaksys
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/ruby_start
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/ruby_logger
@@ -23,14 +38,6 @@ define RUBYFPV_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(@D)/ruby_tx_telemetry
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(RUBYFPV_PKGDIR)/files/ruby_stop.sh
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/sbin $(RUBYFPV_PKGDIR)/files/version_ruby_base.txt
-
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(DATALINK_PKGDIR)/files/tweaksys
-
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/root/ruby/licenses
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/root/ruby/licenses $(@D)/licenses/*
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware/ath9k_htc
-	cp $(RUBYFPV_PKGDIR)/files/htc_9271.fw.3 $(TARGET_DIR)/lib/firmware/ath9k_htc 
 endef
 
 $(eval $(generic-package))
