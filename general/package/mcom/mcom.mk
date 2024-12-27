5d87a96935d20f8b3d6c34a3fbb709637f0f31a2
Ivan Ivanov <purepvll@gmail.com>
2024-12-27 22:45:11 +0300

    Add telemtry init.d service, add socat, mcom utils, configure eth0

--- general/package/mcom/mcom.mk
+++ general/package/mcom/mcom.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
+BASE_PATH = /home/pavel
 
 define MCOM_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 /home/pavel/openipc-firmware/general/package/mcom/microcomV2 $(TARGET_DIR)/usr/bin/mcom
+    $(INSTALL) -D -m 0755 $(BASE_PATH)/openipc-firmware/general/package/mcom/files/socat $(TARGET_DIR)/usr/bin/socat
+    $(INSTALL) -D -m 0755 $(BASE_PATH)/openipc-firmware/general/package/mcom/files/microcomV2 $(TARGET_DIR)/usr/bin/mcom
 endef
 
 $(eval $(generic-package))
