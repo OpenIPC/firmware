71a6884a98bf92113f4e9b7b9d6ad57d1da16139
Signor Pellegrino <68112357+flyrouter@users.noreply.github.com>
2025-02-08 20:22:24 +0300

    Flyrouter patch 1 (#1700)

--- general/package/w1-ds18b20/w1-ds18b20.mk
+++ general/package/w1-ds18b20/w1-ds18b20.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-W1-DS18B20_SITE_METHOD = local
-W1-DS18B20_SITE = $(W1-DS18B20_PKGDIR)/src
+W1_DS18B20_SITE_METHOD = local
+W1_DS18B20_SITE = $(W1_DS18B20_PKGDIR)/src
 
-W1-DS18B20_LICENSE = MIT
-W1-DS18B20_LICENSE_FILES = LICENSE
+W1_DS18B20_LICENSE = MIT
+W1_DS18B20_LICENSE_FILES = LICENSE
 
-define W1-DS18B20_BUILD_CMDS
+define W1_DS18B20_BUILD_CMDS
 	$(TARGET_CC) $(@D)/w1-ds18b20.c -o $(@D)/w1-ds18b20 -Os -s
 endef
 
-define W1-DS18B20_INSTALL_TARGET_CMDS
+define W1_DS18B20_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/bin $(@D)/w1-ds18b20
 endef
 
