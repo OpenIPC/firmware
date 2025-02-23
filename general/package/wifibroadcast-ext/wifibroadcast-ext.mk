b99a38a1705f7bfdd0c422150951763cbd29b0b7
snokvist <joakim.snokvist@gmail.com>
2025-02-23 20:09:21 +0100

    Update wifibroadcast-ext.mk

--- general/package/wifibroadcast-ext/wifibroadcast-ext.mk
+++ general/package/wifibroadcast-ext/wifibroadcast-ext.mk
@@ -13,6 +13,10 @@ endef
 define WIFIBROADCAST_EXT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S75generate_vtx_info
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S98wifibroadcast
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S990provisioner_bind
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S992alink
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S993temperature_check
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/keygen
@@ -28,19 +32,15 @@ define WIFIBROADCAST_EXT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(WIFIBROADCAST_EXT_PKGDIR)/files/temperature_check.sh
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(WIFIBROADCAST_EXT_PKGDIR)/files/wifibroadcast
 
-  ##/etc/
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(WIFIBROADCAST_EXT_PKGDIR)/files/files_to_backup.txt
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(WIFIBROADCAST_EXT_PKGDIR)/files/rc.local
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(WIFIBROADCAST_EXT_PKGDIR)/files/vtx_profiles.yaml
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(WIFIBROADCAST_EXT_PKGDIR)/files/wifi_profiles.yaml
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(WIFIBROADCAST_EXT_PKGDIR)/files/wfb.yaml
 
-  ##/etc/init.d
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S75generate_vtx_info
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S98wifibroadcast
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S990provisioner_bind
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S992alink
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WIFIBROADCAST_EXT_PKGDIR)/files/S993temperature_check
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/var/www
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/var/www $(WIFIBROADCAST_NG_PKGDIR)/www/*
+
 endef
 
 $(eval $(generic-package))
