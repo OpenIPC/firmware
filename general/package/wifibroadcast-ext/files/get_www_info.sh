f99d5f7a83b193ee353a12333b416ba4e0341a12
snokvist <joakim.snokvist@gmail.com>
2025-02-23 11:33:38 +0100

    Delete general/package/wifibroadcast-ext/files/get_www_info.sh

--- general/package/wifibroadcast-ext/files/get_www_info.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-##Add code to print out temp etc for the web interface.
-#soc temp, wifi temp, current wfb settings, current wifi tx_pwr
-ipcinfo -t >> /tmp/vtx.log
-ipcinfo -t > /tmp/www_info
