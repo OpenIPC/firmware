0f119da598c47b418376be74eb33861a3e326f67
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-05-29 20:54:52 +0200

    [no ci] Package: update rtl88x2eu-openipc

--- general/package/rtl88x2eu-openipc/rtl88x2eu-openipc.mk
+++ general/package/rtl88x2eu-openipc/rtl88x2eu-openipc.mk
@@ -4,14 +4,8 @@
 #
 ################################################################################
 
-ifeq ($(LOCAL_DOWNLOAD),y)
-RTL88X2EU_OPENIPC_SITE_METHOD = git
-RTL88X2EU_OPENIPC_SITE = https://github.com/libc0607/rtl88x2eu-20230815
-RTL88X2EU_OPENIPC_VERSION = $(shell git ls-remote $(RTL88X2EU_OPENIPC_SITE) HEAD | head -1 | cut -f1)
-else
-RTL88X2EU_OPENIPC_SITE = https://github.com/libc0607/rtl88x2eu-20230815/archive
-RTL88X2EU_OPENIPC_SOURCE = master.tar.gz
-endif
+RTL88X2EU_OPENIPC_SITE = $(call github,libc0607,rtl88x2eu-20230815,$(RTL88X2EU_OPENIPC_VERSION))
+RTL88X2EU_OPENIPC_VERSION = HEAD
 
 RTL88X2EU_OPENIPC_LICENSE = GPL-2.0
 RTL88X2EU_OPENIPC_LICENSE_FILES = COPYING
