e055f9b669c390f19830cd8bb62b461bf829a466
wangjianxin <wangjianxin@canaan-creative.com>
2025-05-29 17:35:24 +0800

    k230

--- general/package/canaan_k230_sdk/canaan_k230_sdk.mk
+++ general/package/canaan_k230_sdk/canaan_k230_sdk.mk
@@ -20,9 +20,14 @@ CANAAN_K230_SDK_EXTRA_DOWNLOADS += $(k230_download_url)/rootfs.tar.xz
 define CANAAN_K230_SDK_INSTALL_TARGET_CMDS
 	tar -xf $(CANAAN_K230_SDK_DL_DIR)/rootfs.tar.xz -C $(TARGET_DIR)
 	rm -rf $(TARGET_DIR)/var/run;
+	rm -rf $(TARGET_DIR)/etc/init.d/S30dbus;
+	rm -rf $(TARGET_DIR)/etc/init.d/S40bluetoothd;
+	rm -rf $(TARGET_DIR)/etc/init.d/S48sntp;
+	rm -rf $(TARGET_DIR)/etc/init.d/S49ntp;
+	rm -rf $(TARGET_DIR)/etc/init.d/S50crond;
+	rm -rf $(TARGET_DIR)/etc/init.d/S50sshd;
+	rm -rf $(TARGET_DIR)/etc/init.d/S50telnet;
 	gunzip -f -k  -c $(CANAAN_K230_SDK_DL_DIR)/sysimage-sdcard.img.gz > $(BINARIES_DIR)/sysimage-sdcard.img
 endef
-
-
 
 $(eval $(generic-package))
