8cecbbc95410bacb6d2f0e89c62bf7a485202af5
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-07-01 14:43:08 +0200

    Update rtl8189es-openipc.mk (#1834)

--- general/package/rtl8189es-openipc/rtl8189es-openipc.mk
+++ general/package/rtl8189es-openipc/rtl8189es-openipc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 RTL8189ES_OPENIPC_SITE = $(call github,jwrdegoede,rtl8189es_linux,$(RTL8189ES_OPENIPC_VERSION))
-RTL8189ES_OPENIPC_VERSION = HEAD
+RTL8189ES_OPENIPC_VERSION = fcf2a5746e6fe11d9d71337ee5dac6cf43423a97
 
 RTL8189ES_OPENIPC_LICENSE = GPL-2.0
 RTL8189ES_OPENIPC_LICENSE_FILES = COPYING
