c6bf05fb1c3bb753d5891e27ccec04184bf6e262
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-08-09 13:13:52 +0200

    Ingenic: initial support for T23 (#1853)

---
+++ general/package/ingenic-osdrv-t23/ingenic-osdrv-t23.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# ingenic-osdrv-t23
+#
+################################################################################
+
+INGENIC_OSDRV_T23_VERSION =
+INGENIC_OSDRV_T23_SITE =
+INGENIC_OSDRV_T23_LICENSE = MIT
+INGENIC_OSDRV_T23_LICENSE_FILES = LICENSE
+
+define INGENIC_OSDRV_T23_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/*.yaml
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T23_PKGDIR)/files/sensor/params/*.bin
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T23_PKGDIR)/files/script/*
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(INGENIC_OSDRV_T23_PKGDIR)/files/lib/*
+endef
+
+$(eval $(generic-package))
