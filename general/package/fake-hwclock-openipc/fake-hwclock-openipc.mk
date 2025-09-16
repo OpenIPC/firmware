bc40af0dfd3fec1b7c2bb7ea2053d19e815385bd
Daniel Banar <daniel.banar5@gmail.com>
2025-09-16 19:05:07 +0200

    added simple fake-hwclock

---
+++ general/package/fake-hwclock-openipc/fake-hwclock-openipc.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# fake-hwclock-openipc
+#
+################################################################################
+
+define FAKE_HWCLOCK_OPENIPC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(FAKE_HWCLOCK_OPENIPC_PKGDIR)/files/S39fakehwclock
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(FAKE_HWCLOCK_OPENIPC_PKGDIR)/files/fake-hwclock
+endef
+
+$(eval $(generic-package))
