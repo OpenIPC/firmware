619a13c51b071c36cc1582f0a78bbc0655da5f7a
Daniel Banar <daniel.banar5@gmail.com>
2025-09-22 15:18:47 +0200

    removed openipc suffix from package name

--- general/package/fake-hwclock/fake-hwclock.mk
+++ general/package/fake-hwclock/fake-hwclock.mk
@@ -1,15 +1,15 @@
 ################################################################################
 #
-# fake-hwclock-openipc
+# fake-hwclock
 #
 ################################################################################
 
-define FAKE_HWCLOCK_OPENIPC_INSTALL_TARGET_CMDS
+define FAKE_HWCLOCK_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(FAKE_HWCLOCK_OPENIPC_PKGDIR)/files/S39fakehwclock
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(FAKE_HWCLOCK_PKGDIR)/files/S39fakehwclock
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(FAKE_HWCLOCK_OPENIPC_PKGDIR)/files/fake-hwclock
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(FAKE_HWCLOCK_PKGDIR)/files/fake-hwclock
 endef
 
 $(eval $(generic-package))
