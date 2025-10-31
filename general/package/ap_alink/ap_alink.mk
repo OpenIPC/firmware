c3e55dc2b0277478fbd69b93ba180e086c8f01b9
Signor Pellegrino <68112357+flyrouter@users.noreply.github.com>
2025-10-31 12:06:38 +0300

    [no ci] Refactor APALINK_SITE and clean up comments

--- general/package/ap_alink/ap_alink.mk
+++ general/package/ap_alink/ap_alink.mk
@@ -1,12 +1,10 @@
 ################################################################################
-# ap_alink - Buildroot package using upstream Makefile from GitHub
+# apalink
 ################################################################################
 
 APALINK_VERSION = 1.2
-APALINK_SITE = https://github.com/carabidulebabat/AP-ALINK-FPV.git
-APALINK_SITE_METHOD = git
+APALINK_SITE = https://github.com/carabidulebabat/AP-ALINK-FPV.git(call github,carabidulebabat,AP-ALINK-FPV,$(APALINK_VERSION))
 
-# Build (uses the Makefile from the repo)
 define APALINK_BUILD_CMDS
 	$(MAKE) -C $(@D) \
 		CC="$(TARGET_CC)" \
@@ -14,14 +12,9 @@ define APALINK_BUILD_CMDS
 		LDFLAGS="$(TARGET_LDFLAGS)"
 endef
 
-# Install (binary + optional config file)
 define APALINK_INSTALL_TARGET_CMDS
-	# Installe le binaire compilé
 	$(INSTALL) -D -m 0755 $(@D)/ap_alink $(TARGET_DIR)/usr/bin/ap_alink
-
-	# Si tu ajoutes un fichier de conf dans ton dépôt (ex: ap_alink.conf)
-	# décommente cette ligne :
-	 $(INSTALL) -D -m 0644 $(@D)/ap_alink.conf $(TARGET_DIR)/etc/ap_alink.conf
+	$(INSTALL) -D -m 0644 $(@D)/ap_alink.conf $(TARGET_DIR)/etc/ap_alink.conf
 endef
 
 $(eval $(generic-package))
