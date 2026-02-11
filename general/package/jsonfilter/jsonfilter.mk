d4404037bccda02125a6bbbc53751a6deac1627e
viktorxda <35473052+viktorxda@users.noreply.github.com>
2026-02-11 23:14:33 +0100

    [no ci] jsonfilter: set version to specific commit (#1942)

--- general/package/jsonfilter/jsonfilter.mk
+++ general/package/jsonfilter/jsonfilter.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 JSONFILTER_SITE = $(call github,openwrt,jsonpath,$(JSONFILTER_VERSION))
-JSONFILTER_VERSION = HEAD
+JSONFILTER_VERSION = afe72ad6414e0faedd3d780d9328e514b0083cae
 
 JSONFILTER_LICENSE = ISC, BSD-3-Clause
 JSONFILTER_DEPENDENCIES = json-c libubox
