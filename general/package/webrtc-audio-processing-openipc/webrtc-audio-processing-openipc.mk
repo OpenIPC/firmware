014b7fc1e94094efb59beddd016fdf69568e372b
Ystinia <94921687+ystinia@users.noreply.github.com>
2026-03-23 07:40:22 +0100

    [no ci] Update webrtc-audio package (#1960)

--- general/package/webrtc-audio-processing-openipc/webrtc-audio-processing-openipc.mk
+++ general/package/webrtc-audio-processing-openipc/webrtc-audio-processing-openipc.mk
@@ -1,12 +1,14 @@
 ################################################################################
 #
-# webrtc-audio-processing-openipc
+# Copyright (c) OpenIPC  https://openipc.org  MIT License
+#
+# webrtc-audio-processing-openipc — webrtc-audio-processing-openipc Buildroot package
 #
 ################################################################################
 
 WEBRTC_AUDIO_PROCESSING_OPENIPC_VERSION = 0.3.1
 WEBRTC_AUDIO_PROCESSING_OPENIPC_SOURCE = webrtc-audio-processing-$(WEBRTC_AUDIO_PROCESSING_OPENIPC_VERSION).tar.xz
-WEBRTC_AUDIO_PROCESSING_OPENIPC_SITE = http://freedesktop.org/software/pulseaudio/webrtc-audio-processing
+WEBRTC_AUDIO_PROCESSING_OPENIPC_SITE = http://distributions.freedesktop.org/software/pulseaudio/webrtc-audio-processing
 WEBRTC_AUDIO_PROCESSING_OPENIPC_INSTALL_STAGING = YES
 WEBRTC_AUDIO_PROCESSING_OPENIPC_LICENSE = BSD-3-Clause
 WEBRTC_AUDIO_PROCESSING_OPENIPC_LICENSE_FILES = COPYING
