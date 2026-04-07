3a301ef65b374c49e461ab7ce69621b1d2e46c21
viktorxda <35473052+viktorxda@users.noreply.github.com>
2026-04-07 17:20:19 +0200

    Package: add sensor selection for sigmastar osdrv (#1985)

--- general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
+++ general/package/sigmastar-osdrv-infinity6e/sigmastar-osdrv-infinity6e.mk
@@ -17,7 +17,7 @@ define SIGMASTAR_OSDRV_INFINITY6E_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/firmware $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/firmware/*
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensors
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/configs/*
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensors $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/sensor/configs/$(if $(OPENIPC_SNS_MODEL),$(OPENIPC_SNS_MODEL),*).bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(SIGMASTAR_OSDRV_INFINITY6E_PKGDIR)/files/script/*
