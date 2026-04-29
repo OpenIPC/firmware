ba06e20e8848b50f47372447cb214811b8486da2
Vasiliy Yakovlev <vixand@openipc.org>
2026-04-29 21:34:30 +0300

    hisilicon-opensdk: bump SHA pin to 0f35151 (cv300 module_init/exit)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
-HISILICON_OPENSDK_VERSION = 75fac96
+HISILICON_OPENSDK_VERSION = 0f35151
 
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
