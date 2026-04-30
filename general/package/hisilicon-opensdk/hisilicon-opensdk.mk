5541527a4dece8312dd3e68f4232e55a3c3f8228
Vasiliy Yakovlev <vixand@openipc.org>
2026-04-30 13:20:43 +0300

    hisilicon-opensdk: bump SHA pin to 9698276 (cv300 #69 merged)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
-HISILICON_OPENSDK_VERSION = df181c0
+HISILICON_OPENSDK_VERSION = 9698276
 
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
