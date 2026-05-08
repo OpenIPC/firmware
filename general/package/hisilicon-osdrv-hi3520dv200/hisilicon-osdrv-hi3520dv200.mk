ea7a66327281080585c80b88c2b5cdaa4f48fe1e
Dmitry Ilyin <6576495+widgetii@users.noreply.github.com>
2026-05-08 13:01:55 +0300

    hi3520dv200: migrate from Linux 3.0.8 to 4.9.37 (#2075)

--- general/package/hisilicon-osdrv-hi3520dv200/hisilicon-osdrv-hi3520dv200.mk
+++ general/package/hisilicon-osdrv-hi3520dv200/hisilicon-osdrv-hi3520dv200.mk
@@ -8,19 +8,18 @@ HISILICON_OSDRV_HI3520DV200_VERSION =
 HISILICON_OSDRV_HI3520DV200_SITE =
 HISILICON_OSDRV_HI3520DV200_LICENSE = MIT
 HISILICON_OSDRV_HI3520DV200_LICENSE_FILES = LICENSE
-HISILICON_OSDRV_HI3520DV200_DEPENDENCIES = uclibc-compat
 
 # Blob install steps are guarded so the build does not fail before the
 # vendor .ko/.so are dropped under files/kmod and files/lib. The package
 # always installs the load_hisilicon script regardless.
 define HISILICON_OSDRV_HI3520DV200_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.0.8/hisilicon
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.9.37/hisilicon
 	if ls $(HISILICON_OSDRV_HI3520DV200_PKGDIR)/files/kmod/*.ko >/dev/null 2>&1; then \
-		$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.8/hisilicon \
+		$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon \
 			$(HISILICON_OSDRV_HI3520DV200_PKGDIR)/files/kmod/*.ko; \
 	fi
 	if ls $(HISILICON_OSDRV_HI3520DV200_PKGDIR)/files/kmod/extdrv/*.ko >/dev/null 2>&1; then \
-		$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.0.8/hisilicon \
+		$(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/4.9.37/hisilicon \
 			$(HISILICON_OSDRV_HI3520DV200_PKGDIR)/files/kmod/extdrv/*.ko; \
 	fi
 
