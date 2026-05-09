a1effc55763fecf791a33c532dc7eadee5f326b4
Dmitry Ilyin <6576495+widgetii@users.noreply.github.com>
2026-05-09 17:28:43 +0300

    hisilicon-opensdk: bump pin to 80277b8 (base COMM_exit_proc_ctrl strip fix)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK_VERSION))
-HISILICON_OPENSDK_VERSION = f54bea2
+HISILICON_OPENSDK_VERSION = 80277b8
 
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
