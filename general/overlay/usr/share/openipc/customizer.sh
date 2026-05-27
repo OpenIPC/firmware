f73be9471002d1c3e3f3122cc4140d3230b297b3
Conke Hu <conke.hu@maxwit.com>
2026-05-27 09:27:12 +0800

    add /usr/share/openipc/customizer.sh
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SN=$(fw_printenv -n SN)
+if [ ! -z "$SN" ]; then
+    setnetwork -h IPC-$SN
+fi
+
+wled_pin=$(gpiofind wled_pin | awk '{print $2}')
+ircut_pin1=$(gpiofind ircut_pin1 | awk '{print $2}')
+ircut_pin2=$(gpiofind ircut_pin2 | awk '{print $2}')
+
+test -z $wled_pin || yaml-cli -s .nightMode.backlightPin: $wled_pin
+test -z $ircut_pin1 || yaml-cli -s .nightMode.irCutPin1: $ircut_pin1
+test -z $ircut_pin2 || yaml-cli -s .nightMode.irCutPin2: $ircut_pin2
