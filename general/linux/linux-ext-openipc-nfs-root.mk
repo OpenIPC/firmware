0c941ca0275615768face3c145ed37f2993c6add
Tiny Labs Inc <elliot@tinylabs.io>
2026-06-12 23:21:48 -0600

    Fix compile error when feature disabled. Start udhcpc daemon when kernel ip autoconf is dhcp. Use static mac_pton helper for older linux kernel support.

--- general/linux/linux-ext-openipc-nfs-root.mk
+++ general/linux/linux-ext-openipc-nfs-root.mk
@@ -13,9 +13,15 @@ define OPENIPC_NFS_ROOT_SYNC_ETHADDR_SOURCE
 		echo 'obj-y += openipc_ethaddr.o' >> $(@D)/drivers/net/ethernet/Makefile
 endef
 
+define OPENIPC_NFS_ROOT_CHECK_KERNEL_SUPPORT
+	grep -qr 'eth_platform_get_mac_address' $(@D)/net $(@D)/include 2>/dev/null || { \
+		echo "openipc-nfs-root requires Linux >= 4.5 or a vendor kernel with eth_platform_get_mac_address() support" >&2; \
+		exit 1; \
+	}
+endef
+
 define OPENIPC_NFS_ROOT_PREPARE_KERNEL
+	$(OPENIPC_NFS_ROOT_CHECK_KERNEL_SUPPORT)
 	$(APPLY_PATCHES) $(@D) $(BR2_EXTERNAL_GENERAL_PATH)/package/openipc-nfs-root/kernel-patches \*.patch
 	$(OPENIPC_NFS_ROOT_SYNC_ETHADDR_SOURCE)
 endef
-
-LINUX_PRE_BUILD_HOOKS += OPENIPC_NFS_ROOT_SYNC_ETHADDR_SOURCE
--- general/package/openipc-nfs-root/files/openipc_ethaddr.c
+++ general/package/openipc-nfs-root/files/openipc_ethaddr.c
@@ -1,21 +1,48 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include <linux/init.h>
 #include <linux/string.h>
-#include <linux/version.h>
+#include <linux/types.h>
 #include <linux/etherdevice.h>
-
-// mac_pton moved in v6.4
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,4,0)
-#include <linux/hex.h>
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,8)
-#include <linux/kernel.h>
-#else
-#error "openipc_ethaddr(): mac_pton not supported in kernel"
-#endif
 
 // Default u-boot env ethaddr on openipc platforms
 static const u8 default_mac[ETH_ALEN] = {0x00,0x00,0x23,0x34,0x45,0x66};
 static u8 openipc_ethaddr[ETH_ALEN] __aligned(2);
+
+static int openipc_hex_to_bin(char c)
+{
+	if (c >= '0' && c <= '9')
+		return c - '0';
+	if (c >= 'a' && c <= 'f')
+		return c - 'a' + 10;
+	if (c >= 'A' && c <= 'F')
+		return c - 'A' + 10;
+
+	return -1;
+}
+
+static bool openipc_mac_pton(const char *str, u8 *mac)
+{
+	int i;
+
+	/* XX:XX:XX:XX:XX:XX */
+	if (strlen(str) != 3 * ETH_ALEN - 1)
+		return false;
+
+	for (i = 0; i < ETH_ALEN; i++) {
+		int hi = openipc_hex_to_bin(str[i * 3]);
+		int lo = openipc_hex_to_bin(str[i * 3 + 1]);
+
+		if (hi < 0 || lo < 0)
+			return false;
+
+		if (i != ETH_ALEN - 1 && str[i * 3 + 2] != ':')
+			return false;
+
+		mac[i] = (hi << 4) | lo;
+	}
+
+	return true;
+}
 
 static int __init openipc_early_ethaddr(char *str)
 {
@@ -24,7 +51,7 @@ static int __init openipc_early_ethaddr(char *str)
 	if (!str)
 		return 0;
 
-	if (!mac_pton(str, mac)) {
+	if (!openipc_mac_pton(str, mac)) {
 		pr_warn("openipc_ethaddr: invalid ethaddr bootarg '%s'\n", str);
 		return 0;
 	}
